146b2b6a6577d7bac96a8694a9a484a4710e80ff
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-03-29 10:13:58 +0000

    Dev

--- infra/13-nfs-subdir-external-provisioner/main.tf
+++ infra/13-nfs-subdir-external-provisioner/main.tf
@@ -14,6 +14,6 @@ resource "helm_release" "nfs_provisioner" {
 
   set {
     name  = "nfs.path"
-    value = "/exports/dynamic-pv"
+    value = "/exports/nfs-client"
   }
 }
--- scripts/port-forward-argocd.sh
+++ scripts/port-forward-argocd.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 opts=""
+port=8443
+
+if [ ! -z $PORT ]; then
+  port=$PORT
+fi
 
 if [ ! -z $BIND_ADDRESS ]; then
   opts="--address $BIND_ADDRESS"
 fi
 
-kubectl -n argocd port-forward svc/argocd-server 8443:443 $opts
+kubectl -n argocd port-forward svc/argocd-server $port:443 $opts
