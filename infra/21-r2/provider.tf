c9412a744476d0d9ae793f9004b9a34adda5486c
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-04-03 22:12:02 +0700

    add r2 stuffs

--- infra/21-r2/provider.tf
+++ infra/21-r2/provider.tf
@@ -11,6 +11,7 @@ provider "aws" {
   skip_credentials_validation = true
   skip_region_validation      = true
   skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
 
   endpoints {
     s3 = "https://${local.cloudlfare_account_id}.r2.cloudflarestorage.com"
--- infra/21-r2/r2.tf
+++ infra/21-r2/r2.tf
@@ -14,18 +14,16 @@ resource "aws_s3_bucket_lifecycle_configuration" "backup" {
       days = 30
     }
   }
-
-  rule {
-    id     = "abort-multipart-upload"
-    status = "Enabled"
-    abort_incomplete_multipart_upload {
-      days_after_initiation = 1
-    }
-  }
 }
 
 resource "cloudflare_r2_bucket" "public" {
   account_id = local.cloudlfare_account_id
   name       = "public"
+  location   = "APAC"
+}
+
+resource "cloudflare_r2_bucket" "stuffs" {
+  account_id = local.cloudlfare_account_id
+  name       = "stuffs"
   location   = "APAC"
 }
