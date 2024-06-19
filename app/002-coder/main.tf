c4b81de3ead155915118f339db9dd0b19b576aaf
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-06-19 08:45:01 +0000

    upgrade coder

--- app/002-coder/main.tf
+++ app/002-coder/main.tf
@@ -5,7 +5,7 @@ resource "helm_release" "coder" {
 
   repository = "https://helm.coder.com/v2"
   chart      = "coder"
-  version    = "2.10.1"
+  version    = "2.12.1"
 
   values = [
     "${file("./values.yaml")}"
