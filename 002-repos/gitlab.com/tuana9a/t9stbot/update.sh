aa87fcacfa2d7804b405831eaa8c9a3aa83e9909
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-07-05 06:47:07 +0000

    refactor: make setting secrets more beautiful

---
+++ 002-repos/gitlab.com/tuana9a/t9stbot/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+var_dir=.s9cr9t
+repo=tuana9a/t9stbot
+
+for x in $(ls $var_dir); do
+    glab variable --repo $repo update --protected --masked $x --value "$(ansible-vault view $var_dir/$x)"
+done
