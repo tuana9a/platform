e54dbb5cdcee7afc2197c8a17931f777542a4db9
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-07-18 17:54:12 +0700

    Coder template: docker image per project (#27)
    * coder: refactor template and add image parameter
    * coder: add more docker image
    * specify the name of github workflow

---
+++ 502-coder/templates/kubernetes/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    coder = {
+      source = "coder/coder"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+provider "coder" {
+}
+
+provider "kubernetes" {
+  # Authenticate via ~/.kube/config or a Coder-specific ServiceAccount, depending on admin preferences
+  config_path = var.use_kubeconfig == true ? "~/.kube/config" : null
+}
