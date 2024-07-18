0e76d4ec8689a4ff7bd6fd90421347bc276c39e4
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-07-18 09:42:37 +0000

    coder: refactor template and add image parameter

---
+++ 502-coder/templates/kubernetes/coder_parameter.data.tf
@@ -0,0 +1,92 @@
+data "coder_parameter" "cpu" {
+  name         = "cpu"
+  display_name = "CPU"
+  description  = "The number of CPU cores"
+  default      = "2"
+  icon         = "/icon/memory.svg"
+  mutable      = true
+  option {
+    name  = "2 Cores"
+    value = "2"
+  }
+  option {
+    name  = "4 Cores"
+    value = "4"
+  }
+  option {
+    name  = "6 Cores"
+    value = "6"
+  }
+  option {
+    name  = "8 Cores"
+    value = "8"
+  }
+}
+
+data "coder_parameter" "memory" {
+  name         = "memory"
+  display_name = "Memory"
+  description  = "The amount of memory in GB"
+  default      = "2"
+  icon         = "/icon/memory.svg"
+  mutable      = true
+  option {
+    name  = "2 GB"
+    value = "2"
+  }
+  option {
+    name  = "4 GB"
+    value = "4"
+  }
+  option {
+    name  = "6 GB"
+    value = "6"
+  }
+  option {
+    name  = "8 GB"
+    value = "8"
+  }
+}
+
+data "coder_parameter" "home_disk_size" {
+  name         = "home_disk_size"
+  display_name = "Home disk size"
+  description  = "The size of the home disk in GB"
+  default      = "2"
+  type         = "number"
+  icon         = "/emojis/1f4be.png"
+  mutable      = false
+  validation {
+    min = 1
+    max = 100
+  }
+}
+
+data "coder_parameter" "image" {
+  name         = "image"
+  display_name = "Image"
+  description  = "Docker image"
+  default      = "tuana9a/coder:base"
+  icon         = "/icon/docker.png"
+  mutable      = true
+  option {
+    name  = "tuana9a/coder:base"
+    value = "tuana9a/coder:base"
+  }
+  option {
+    name  = "tuana9a/coder:adrazaamon-devops"
+    value = "tuana9a/coder:adrazaamon-devops"
+  }
+  option {
+    name  = "tuana9a/coder:nodejs"
+    value = "tuana9a/coder:nodejs"
+  }
+  option {
+    name  = "tuana9a/coder:java"
+    value = "tuana9a/coder:java"
+  }
+  option {
+    name  = "tuana9a/coder:terraform"
+    value = "tuana9a/coder:terraform"
+  }
+}
--- 502-coder/templates/kubernetes/main.tf
+++ 502-coder/templates/kubernetes/main.tf
@@ -1,105 +1,3 @@
-terraform {
-  required_providers {
-    coder = {
-      source = "coder/coder"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-  }
-}
-
-provider "coder" {
-}
-
-variable "use_kubeconfig" {
-  type        = bool
-  description = <<-EOF
-  Use host kubeconfig? (true/false)
-
-  Set this to false if the Coder host is itself running as a Pod on the same
-  Kubernetes cluster as you are deploying workspaces to.
-
-  Set this to true if the Coder host is running outside the Kubernetes cluster
-  for workspaces.  A valid "~/.kube/config" must be present on the Coder host.
-  EOF
-  default     = false
-}
-
-variable "namespace" {
-  type        = string
-  description = "The Kubernetes namespace to create workspaces in (must exist prior to creating workspaces). If the Coder host is itself running as a Pod on the same Kubernetes cluster as you are deploying workspaces to, set this to the same namespace."
-}
-
-data "coder_parameter" "cpu" {
-  name         = "cpu"
-  display_name = "CPU"
-  description  = "The number of CPU cores"
-  default      = "2"
-  icon         = "/icon/memory.svg"
-  mutable      = true
-  option {
-    name  = "2 Cores"
-    value = "2"
-  }
-  option {
-    name  = "4 Cores"
-    value = "4"
-  }
-  option {
-    name  = "6 Cores"
-    value = "6"
-  }
-  option {
-    name  = "8 Cores"
-    value = "8"
-  }
-}
-
-data "coder_parameter" "memory" {
-  name         = "memory"
-  display_name = "Memory"
-  description  = "The amount of memory in GB"
-  default      = "2"
-  icon         = "/icon/memory.svg"
-  mutable      = true
-  option {
-    name  = "2 GB"
-    value = "2"
-  }
-  option {
-    name  = "4 GB"
-    value = "4"
-  }
-  option {
-    name  = "6 GB"
-    value = "6"
-  }
-  option {
-    name  = "8 GB"
-    value = "8"
-  }
-}
-
-data "coder_parameter" "home_disk_size" {
-  name         = "home_disk_size"
-  display_name = "Home disk size"
-  description  = "The size of the home disk in GB"
-  default      = "2"
-  type         = "number"
-  icon         = "/emojis/1f4be.png"
-  mutable      = false
-  validation {
-    min = 1
-    max = 100
-  }
-}
-
-provider "kubernetes" {
-  # Authenticate via ~/.kube/config or a Coder-specific ServiceAccount, depending on admin preferences
-  config_path = var.use_kubeconfig == true ? "~/.kube/config" : null
-}
-
 data "coder_workspace" "me" {}
 
 resource "coder_agent" "main" {
@@ -211,7 +109,6 @@ resource "kubernetes_stateful_set" "main" {
   spec {
     replicas = 1
 
-    # @tuana9a
     service_name = data.coder_workspace.me.name
 
     selector {
@@ -234,7 +131,7 @@ resource "kubernetes_stateful_set" "main" {
 
         container {
           name              = "dev"
-          image             = "tuana9a/coder:base"
+          image             = data.coder_parameter.image.value
           image_pull_policy = "Always"
           command           = ["sh", "-c", coder_agent.main.init_script]
           security_context {
@@ -283,7 +180,6 @@ resource "kubernetes_stateful_set" "main" {
       }
     }
 
-    # @tuana9a
     volume_claim_template {
       metadata {
         name      = "home"
