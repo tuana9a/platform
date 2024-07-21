f86935daa2f8b6066bce07c4c8584f033d215cf2
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-07-21 10:29:23 +0000

    coder: add the fucking docker in docker
    NOTE: mount volume for rootless docker on nfs will not work

---
+++ 502-coder/templates/kubernetes-dockerd/README.md
@@ -0,0 +1,7 @@
+# kubernetes
+
+First push
+
+```bash
+coder template push --variables-file vars.yml
+```
---
+++ 502-coder/templates/kubernetes-dockerd/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    coder = {
+      source = "coder/coder"
+      version = "0.20.1"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.28.1"
+    }
+  }
+}
+
+provider "coder" {
+}
+
+provider "kubernetes" {
+  # Authenticate via ~/.kube/config or a Coder-specific ServiceAccount, depending on admin preferences
+  config_path = var.use_kubeconfig == true ? "~/.kube/config" : null
+}
