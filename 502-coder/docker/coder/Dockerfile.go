a67d84882e9b01951cc9cd71cb995ca4c62634aa
long.duongbao <52832058+longduongxx86@users.noreply.github.com>
2024-07-21 09:12:45 +0700

    update Dockerfile

--- 502-coder/docker/coder/Dockerfile.go
+++ 502-coder/docker/coder/Dockerfile.go
@@ -13,6 +13,6 @@ RUN sudo wget -q https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/insta
 
 RUN sudo wget -q https://go.dev/dl/go1.22.5.linux-amd64.tar.gz -O /opt/go1.22.5.linux-amd64.tar.gz \
     && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /opt/go1.22.5.linux-amd64.tar.gz \
-    && echo "export export PATH=$PATH:/usr/local/go/bin" | sudo tee -a /etc/profile
+    && echo "export PATH=$PATH:/usr/local/go/bin" | sudo tee -a /etc/profile
 
-RUN go version
+RUN /usr/local/go/bin/go version
