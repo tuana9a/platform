a9c9c66415efc38f6e743c9ca8e86f12ffbdad47
long.duongbao <52832058+longduongxx86@users.noreply.github.com>
2024-07-21 10:10:44 +0700

    init go image (#32)
    * init go image
    * coder: add go option
    
    ---------
    
    Co-authored-by: Nguyen Minh Tuan <tuana9a@gmail.com>

---
+++ 502-coder/docker/coder/Dockerfile.go
@@ -0,0 +1,18 @@
+FROM codercom/enterprise-base:ubuntu
+
+USER coder
+
+RUN sudo apt update -y
+
+# direnv
+RUN sudo apt install -y direnv
+
+# zoxide
+RUN sudo wget -q https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh -O /opt/install-zoxide.sh \
+    && sudo chmod +x /opt/install-zoxide.sh && sudo /opt/install-zoxide.sh --bin-dir /usr/local/bin/
+
+RUN sudo wget -q https://go.dev/dl/go1.22.5.linux-amd64.tar.gz -O /opt/go1.22.5.linux-amd64.tar.gz \
+    && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /opt/go1.22.5.linux-amd64.tar.gz \
+    && echo "export PATH=$PATH:/usr/local/go/bin" | sudo tee -a /etc/profile
+
+RUN /usr/local/go/bin/go version
