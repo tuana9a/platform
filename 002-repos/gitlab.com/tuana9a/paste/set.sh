07b3c6cc4f7b90e1a97532b0a6137618598f16f7
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-07-24 12:39:19 +0000

    add: gitlab-ci secrets for tuana9a/paste

---
+++ 002-repos/gitlab.com/tuana9a/paste/set.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+var_dir=.s9cr9t
+repo=tuana9a/paste
+
+for x in $(ls $var_dir); do
+    glab variable --repo $repo set --protected --masked $x --value "$(ansible-vault view $var_dir/$x)"
+done
