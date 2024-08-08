2e2797e710d06ec95ecf062bd113c67edf6ed92f
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-08-08 19:57:17 +0700

    feat(coder): expose coder workspace (#36)

--- 023-dns/dns_coder.tf
+++ 023-dns/dns_coder.tf
@@ -6,3 +6,12 @@ resource "cloudflare_record" "coder" {
   ttl     = 60
   proxied = false
 }
+
+resource "cloudflare_record" "wildcard_coder" {
+  zone_id = data.cloudflare_zones.tuana9a_com.zones[0].id
+  name    = "*.coder"
+  value   = cloudflare_record.zephyrus.hostname
+  type    = "CNAME"
+  ttl     = 60
+  proxied = false
+}
