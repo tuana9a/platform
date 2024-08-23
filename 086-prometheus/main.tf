4f6f9aaaa1d45132c2976c671c8d2bdb29e7bf2d
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-08-23 14:46:19 +0000

    feat: migrate storage class to longhorn

--- 086-prometheus/main.tf
+++ 086-prometheus/main.tf
@@ -1,10 +1,6 @@
-locals {
-  namespace = "prometheus"
-}
-
 resource "helm_release" "prometheus" {
   name             = "prometheus"
-  namespace        = local.namespace
+  namespace        = "prometheus"
   create_namespace = true
 
   repository = "https://prometheus-community.github.io/helm-charts"
@@ -12,20 +8,6 @@ resource "helm_release" "prometheus" {
   version    = "25.20.1"
 
   values = [
-    "${file("./prometheus-values.yaml")}"
-  ]
-}
-
-resource "helm_release" "grafana" {
-  name             = "grafana"
-  namespace        = local.namespace
-  create_namespace = true
-
-  repository = "https://grafana.github.io/helm-charts"
-  chart      = "grafana"
-  version    = "7.3.10"
-
-  values = [
-    "${file("./grafana-values.yaml")}"
+    "${file("./values.yaml")}"
   ]
 }
