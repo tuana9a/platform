20101531d2b90ac547c6f4914c2408a7db393543
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-09-19 12:50:58 +0000

    refactor(dns): shorten dns for knative

--- 023-dns/dns_knative.tf
+++ 023-dns/dns_knative.tf
@@ -1,15 +1,15 @@
 resource "cloudflare_record" "knative_default" {
   zone_id = data.cloudflare_zones.tuana9a_com.zones[0].id
-  name    = "*.default.knative"
+  name    = "*.default.kn"
   value   = cloudflare_record.zione.hostname
   type    = "CNAME"
   ttl     = 60
   proxied = false
 }
 
-resource "cloudflare_record" "knative_dkhptd" {
+resource "cloudflare_record" "knative_tuana9a" {
   zone_id = data.cloudflare_zones.tuana9a_com.zones[0].id
-  name    = "*.dkhptd.knative"
+  name    = "*.tuana9a.kn"
   value   = cloudflare_record.zione.hostname
   type    = "CNAME"
   ttl     = 60
