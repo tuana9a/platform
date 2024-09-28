9dd1cb75fbac68f224aa2c825d014353c36c23c0
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-09-28 05:47:27 +0000

    bye bye longhorn

--- 082-longhorn/main.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "longhorn" {
-  name             = "longhorn"
-  namespace        = "longhorn-system"
-  create_namespace = true
-
-  repository = "https://charts.longhorn.io"
-  chart      = "longhorn"
-  version    = "1.7.0"
-}
