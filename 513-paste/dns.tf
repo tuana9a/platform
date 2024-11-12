402d2a4a8440bd0bf248699632a256ba1e44fc91
Nguyen Minh Tuan <tuana9a@gmail.com>
2024-11-12 14:03:58 +0000

    refactor(paste): add cloud run mapping custom domain
    https://cloud.google.com/run/docs/mapping-custom-domains#terraform

--- 513-paste/dns.tf
+++ 513-paste/dns.tf
@@ -1,8 +1,18 @@
-resource "cloudflare_record" "paste" {
+resource "cloudflare_record" "paste_uri" {
   zone_id = data.cloudflare_zones.tuana9a_com.zones[0].id
   name    = "paste"
   value   = google_cloud_run_v2_service.paste.uri
   type    = "TXT"
+  ttl     = 60
+  proxied = false
+}
+
+# https://console.cloud.google.com/run/domains
+resource "cloudflare_record" "paste" {
+  zone_id = data.cloudflare_zones.tuana9a_com.zones[0].id
+  name    = "paste"
+  value   = "ghs.googlehosted.com."
+  type    = "CNAME"
   ttl     = 60
   proxied = false
 }
--- 513-paste/output.tf
+++ 513-paste/output.tf
@@ -1,7 +1,3 @@
 output "uri" {
   value = google_cloud_run_v2_service.paste.uri
 }
-
-output "cmd" {
-  value = "dig -t txt ${cloudflare_record.paste.hostname}"
-}
