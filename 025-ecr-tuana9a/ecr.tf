30044817b90e88d0f5b2965a4a37269b98c1ddaf
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-01-01 09:55:30 +0000

    add: ecr

---
+++ 025-ecr-tuana9a/ecr.tf
@@ -0,0 +1,4 @@
+# resource "aws_ecr_pull_through_cache_rule" "k8s" {
+#   ecr_repository_prefix = "k8s"
+#   upstream_registry_url = "registry.k8s.io"
+# }
---
+++ 025-ecr-tuana9a/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-tuana9a"
+    prefix = "025-ecr-tuana9a"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.29.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "google" {
+  project = var.gcp_project_name
+  region  = var.gcp_region_name
+  zone    = var.gcp_zone_name
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
