6923384dd82d66f7a6bcc34dc313a9e547b909a6
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-01-19 05:21:01 +0000

    refactor: migrate k8s-ingress-lb to xenomorph

--- templates/update-ingress-nginx-load-balancer-ip.sh
+++ templates/update-ingress-nginx-load-balancer-ip.sh
@@ -7,8 +7,9 @@ echo "=== $(date) ==="
 
 namespace="{{ namespace | default('ingress-nginx') }}"
 nginx_stream_conf_file="/etc/nginx/stream.conf.d/ingress_nginx_load_balancer.conf"
+overwrite_flag=$(cat /tmp/overwrite-flag)
 
-current_ip=$(kubectl -n $namespace get service ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+current_ip=$(kubectl --kubeconfig /opt/kubeconfig -n $namespace get service ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 if [ -z $current_ip ]; then
   echo "invalid current_ip \"$current_ip\""
@@ -18,24 +19,24 @@ fi
 
 previous_ip=$(cat $nginx_stream_conf_file | grep -oP '\d+\.\d+\.\d+\.\d+' | head -n 1)
 
-if [ $current_ip == $previous_ip  ]; then
+if [ $overwrite_flag == "1" && $current_ip == $previous_ip ]; then
   echo "\"$current_ip\" == \"$previous_ip\" (no changes)"
-  echo "exit 0"
   exit 0
 fi
 
 echo "\"$current_ip\" != \"$previous_ip\" (diff)"
 echo "update nginx config"
 
-echo "# $(date)
-# This file $nginx_stream_conf_file is generated and will be overwrited
+config='# '$(date)'
+# This file '$nginx_stream_conf_file' is generated and will be overwrited
 server {
   listen 80;
-  proxy_pass $current_ip:80;
+  proxy_pass '$current_ip':80;
 }
 server {
   listen 443;
-  proxy_pass $current_ip:443;
-}" | sudo tee $nginx_stream_conf_file
+  proxy_pass '$current_ip':443;
+}'
 
-sudo systemctl reload nginx
+echo "$config" | tee $nginx_stream_conf_file
+systemctl reload nginx
