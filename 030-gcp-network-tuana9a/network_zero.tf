aadef559e697697a4043ac93bb97330ba8945320
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-02-01 12:49:21 +0000

    fix

--- 030-gcp-network-tuana9a/network_zero.tf
+++ 030-gcp-network-tuana9a/network_zero.tf
@@ -15,6 +15,6 @@ resource "google_compute_subnetwork" "zero_singapore" {
 resource "google_compute_subnetwork" "zero_tokyo" {
   name          = google_compute_network.zero.name
   ip_cidr_range = "10.1.0.0/16"
-  region        = "asia-northeast1-a"
+  region        = "asia-northeast1"
   network       = google_compute_network.zero.id
 }
