5aa945a10d916e8018536162710be64511c65cbc
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-02-01 10:34:44 +0000

    add(gcp): service accounts: terraform state editor, terraform network editor

---
+++ 005-iam-service-account-gcp-tuana9a/svc_acc_terraform_network_admin.tf
@@ -0,0 +1,30 @@
+resource "google_service_account" "terraform_network_admin" {
+  account_id = "terraform-network-admin"
+}
+
+resource "google_project_iam_member" "terraform_network_admin_1" {
+  project = var.gcp_project_name
+  # https://cloud.google.com/storage/docs/access-control/iam-roles
+  role   = "roles/storage.objectUser"
+  member = "serviceAccount:${google_service_account.terraform_network_admin.email}"
+  condition {
+    title      = "restrict_bucket_terraform_tuana9a"
+    expression = "resource.name.startsWith('projects/${data.google_project.current.name}/buckets/terraform-tuana9a')"
+  }
+}
+
+resource "google_project_iam_member" "terraform_network_admin_2" {
+  project = var.gcp_project_name
+  # https://cloud.google.com/storage/docs/access-control/iam-roles
+  role   = "roles/compute.networkAdmin"
+  member = "serviceAccount:${google_service_account.terraform_network_admin.email}"
+}
+
+resource "google_service_account_iam_binding" "terraform_network_admin_claims" {
+  service_account_id = google_service_account.terraform_network_admin.name
+  role               = "roles/iam.workloadIdentityUser"
+  members = [
+    # See https://github.com/google-github-actions/auth?tab=readme-ov-file#workload-identity-federation-through-a-service-account
+    "principalSet://iam.googleapis.com/projects/${data.google_project.current.number}/locations/global/workloadIdentityPools/${data.google_iam_workload_identity_pool.github.workload_identity_pool_id}/attribute.repository/tuana9a/platform",
+  ]
+}
---
+++ 005-iam-service-account-gcp-tuana9a/terraform.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-tuana9a"
+    prefix = "005-iam-service-account-gcp-tuana9a"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.28.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "5.34.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_name
+  region  = var.gcp_region_name
+  zone    = var.gcp_zone_name
+}
+
+provider "google-beta" {
+  project = var.gcp_project_name
+  region  = var.gcp_region_name
+  zone    = var.gcp_zone_name
+}
