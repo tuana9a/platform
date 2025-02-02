a4a8696cab94be86a94325a6b8290d7b13fd1ee4
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-02-02 02:52:53 +0000

    test edit ecr

--- 025-ecr-tuana9a/ecr.tf
+++ 025-ecr-tuana9a/ecr.tf
@@ -1,4 +1,55 @@
-# resource "aws_ecr_pull_through_cache_rule" "k8s" {
-#   ecr_repository_prefix = "k8s"
-#   upstream_registry_url = "registry.k8s.io"
-# }
+resource "aws_ecr_repository" "registry_k8s_io" {
+  name = "registry.k8s.io"
+
+  image_tag_mutability = true
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+data "aws_iam_policy_document" "registry_k8s_io" {
+  statement {
+    sid    = "allow-pull"
+    effect = "Allow"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+    ]
+  }
+
+  statement {
+    sid    = "allow-push"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${data.aws_caller_identity.current.account_id}"]
+    }
+
+    actions = [
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "registry_k8s_io" {
+  repository = aws_ecr_repository.registry_k8s_io.name
+  policy     = data.aws_iam_policy_document.exregistry_k8s_ioample.json
+}
