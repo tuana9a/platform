ac948cfe623920c82f7500ba33a4d3e9d6442ecb
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-02-07 15:48:48 +0000

    fix: re-add pve2.tuana9a.com for proxmox cert renewal

--- 023-dns/dns_xenomorph.tf
+++ 023-dns/dns_xenomorph.tf
@@ -6,3 +6,12 @@ resource "cloudflare_record" "xenomorph" {
   ttl     = 60
   proxied = false
 }
+
+resource "cloudflare_record" "pve2" {
+  zone_id = data.cloudflare_zone.tuana9a_com.id
+  name    = "pve2"
+  value   = data.vault_kv_secret.vhost_dual_x5650_public_ip.data.ip
+  type    = "A"
+  ttl     = 60
+  proxied = false
+}
