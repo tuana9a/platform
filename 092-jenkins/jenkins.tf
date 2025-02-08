421880b75bdf3efa43a8f0568ffa933e9b9bd402
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-02-08 01:15:14 +0000

    add: jenkins

---
+++ 092-jenkins/jenkins.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "jenkins" {
+  name             = "jenkins"
+  namespace        = "jenkins"
+  create_namespace = true
+
+  repository = "https://charts.jenkins.io"
+  chart      = "jenkins"
+  version    = "5.8.10"
+
+  values = [file("./values.yml")]
+}
