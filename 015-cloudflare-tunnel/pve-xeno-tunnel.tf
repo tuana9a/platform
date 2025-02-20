17e6c1490eb869b5887ef5d0c647f23fdc4aa02b
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-02-20 08:29:01 +0000

    change(code-server): migrate k8s forward style instead of cloudflare tunnel

--- 015-cloudflare-tunnel/pve-xeno-tunnel.tf
+++ 015-cloudflare-tunnel/pve-xeno-tunnel.tf
@@ -4,46 +4,32 @@ data "cloudflare_zero_trust_tunnel_cloudflared" "pve_xeno_tunnel" {
   name       = "pve-xeno-tunnel"
 }
 
-import {
-  id = "${data.cloudflare_zone.tuana9a_com.id}/41d0f9574c7d817ba368aa2c3b4cc2af"
-  to = cloudflare_record.dev2_tuana9a_com
-}
+# resource "cloudflare_record" "dev2_tuana9a_com" {
+#   zone_id = data.cloudflare_zone.tuana9a_com.id
+#   name    = "dev2"
+#   content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
+#   type    = "CNAME"
+#   ttl     = 1
+#   proxied = true
+# }
 
-resource "cloudflare_record" "dev2_tuana9a_com" {
-  zone_id = data.cloudflare_zone.tuana9a_com.id
-  name    = "dev2"
-  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
+# resource "cloudflare_record" "amon_tuana9a_com" {
+#   zone_id = data.cloudflare_zone.tuana9a_com.id
+#   name    = "amon"
+#   content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
+#   type    = "CNAME"
+#   ttl     = 1
+#   proxied = true
+# }
 
-resource "cloudflare_record" "dev2-8000_tuana9a_com" {
-  zone_id = data.cloudflare_zone.tuana9a_com.id
-  name    = "dev2-8000"
-  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
-
-resource "cloudflare_record" "dev2-8080_tuana9a_com" {
-  zone_id = data.cloudflare_zone.tuana9a_com.id
-  name    = "dev2-8080"
-  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
-
-resource "cloudflare_record" "dev2-9090_tuana9a_com" {
-  zone_id = data.cloudflare_zone.tuana9a_com.id
-  name    = "dev2-9090"
-  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
+# resource "cloudflare_record" "pve-xeno_tuana9a_com" {
+#   zone_id = data.cloudflare_zone.tuana9a_com.id
+#   name    = "pve-xeno"
+#   content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
+#   type    = "CNAME"
+#   ttl     = 1
+#   proxied = true
+# }
 
 resource "cloudflare_record" "ssh-dev2_tuana9a_com" {
   zone_id = data.cloudflare_zone.tuana9a_com.id
@@ -54,36 +40,19 @@ resource "cloudflare_record" "ssh-dev2_tuana9a_com" {
   proxied = true
 }
 
-resource "cloudflare_record" "amon_tuana9a_com" {
+resource "cloudflare_zero_trust_access_application" "ssh-dev2" {
   zone_id = data.cloudflare_zone.tuana9a_com.id
-  name    = "amon"
-  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
+  name    = "ssh-dev2"
+  domain  = cloudflare_record.ssh-dev2_tuana9a_com.hostname
+  type    = "ssh"
 
-resource "cloudflare_record" "amon-8000_tuana9a_com" {
-  zone_id = data.cloudflare_zone.tuana9a_com.id
-  name    = "amon-8000"
-  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
+  session_duration = "24h"
 
-resource "cloudflare_record" "pve-xeno_tuana9a_com" {
-  zone_id = data.cloudflare_zone.tuana9a_com.id
-  name    = "pve-xeno"
-  content = "${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}.cfargotunnel.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
+  auto_redirect_to_identity = false
 
-import {
-  id = "${local.cloudflare_account_id}/${data.cloudflare_zero_trust_tunnel_cloudflared.pve_xeno_tunnel.id}"
-  to = cloudflare_zero_trust_tunnel_cloudflared_config.pve_xeno_tunnel
+  policies = [
+    cloudflare_zero_trust_access_policy.allow_tuana9a.id,
+  ]
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared_config" "pve_xeno_tunnel" {
@@ -94,110 +63,58 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "pve_xeno_tunnel" {
     warp_routing {
       enabled = true
     }
-    ingress_rule {
-      hostname = cloudflare_record.pve-xeno_tuana9a_com.hostname
-      service  = "https://192.168.56.1:8006"
-      origin_request {
-        bastion_mode             = false
-        disable_chunked_encoding = false
-        http2_origin             = false
-        keep_alive_connections   = 0
-        no_happy_eyeballs        = false
-        no_tls_verify            = true
-        proxy_port               = 0
-      }
-    }
-    ingress_rule {
-      hostname = cloudflare_record.dev2_tuana9a_com.hostname
-      service  = "http://192.168.56.209:8209"
-      origin_request {
-        bastion_mode             = false
-        disable_chunked_encoding = false
-        http2_origin             = false
-        keep_alive_connections   = 0
-        no_happy_eyeballs        = false
-        no_tls_verify            = false
-        proxy_port               = 0
-      }
-    }
-    ingress_rule {
-      hostname = cloudflare_record.dev2-8000_tuana9a_com.hostname
-      service  = "http://192.168.56.209:8000"
-      origin_request {
-        bastion_mode             = false
-        disable_chunked_encoding = false
-        http2_origin             = false
-        keep_alive_connections   = 0
-        no_happy_eyeballs        = false
-        no_tls_verify            = false
-        proxy_port               = 0
-      }
-    }
-    ingress_rule {
-      hostname = cloudflare_record.dev2-8080_tuana9a_com.hostname
-      service  = "http://192.168.56.209:8080"
-      origin_request {
-        bastion_mode             = false
-        disable_chunked_encoding = false
-        http2_origin             = false
-        keep_alive_connections   = 0
-        no_happy_eyeballs        = false
-        no_tls_verify            = false
-        proxy_port               = 0
-      }
-    }
-    ingress_rule {
-      hostname = cloudflare_record.dev2-9090_tuana9a_com.hostname
-      service  = "http://192.168.56.209:9090"
-      origin_request {
-        bastion_mode             = false
-        disable_chunked_encoding = false
-        http2_origin             = false
-        keep_alive_connections   = 0
-        no_happy_eyeballs        = false
-        no_tls_verify            = false
-        proxy_port               = 0
-      }
-    }
-    ingress_rule {
-      hostname = cloudflare_record.ssh-dev2_tuana9a_com.hostname
-      service  = "ssh://192.168.56.209:22"
-      origin_request {
-        bastion_mode             = false
-        disable_chunked_encoding = false
-        http2_origin             = false
-        keep_alive_connections   = 0
-        no_happy_eyeballs        = false
-        no_tls_verify            = false
-        proxy_port               = 0
-      }
-    }
-    ingress_rule {
-      hostname = cloudflare_record.amon_tuana9a_com.hostname
-      service  = "http://192.168.56.206:8206"
-      origin_request {
-        bastion_mode             = false
-        disable_chunked_encoding = false
-        http2_origin             = false
-        keep_alive_connections   = 0
-        no_happy_eyeballs        = false
-        no_tls_verify            = false
-        proxy_port               = 0
-      }
-    }
-    ingress_rule {
-      hostname = cloudflare_record.amon-8000_tuana9a_com.hostname
-      service  = "http://192.168.56.206:8000"
-      origin_request {
-        bastion_mode             = false
-        disable_chunked_encoding = false
-        http2_origin             = false
-        keep_alive_connections   = 0
-        no_happy_eyeballs        = false
-        no_tls_verify            = false
-        proxy_port               = 0
-      }
-    }
+    # ingress_rule {
+    #   hostname = cloudflare_record.pve-xeno_tuana9a_com.hostname
+    #   service  = "https://192.168.56.1:8006"
+    #   origin_request {
+    #     bastion_mode             = false
+    #     disable_chunked_encoding = false
+    #     http2_origin             = false
+    #     keep_alive_connections   = 0
+    #     no_happy_eyeballs        = false
+    #     no_tls_verify            = true
+    #     proxy_port               = 0
+    #   }
+    # }
+    # ingress_rule {
+    #   hostname = cloudflare_record.dev2_tuana9a_com.hostname
+    #   service  = "http://192.168.56.209:8209"
+    #   origin_request {
+    #     bastion_mode             = false
+    #     disable_chunked_encoding = false
+    #     http2_origin             = false
+    #     keep_alive_connections   = 0
+    #     no_happy_eyeballs        = false
+    #     no_tls_verify            = false
+    #     proxy_port               = 0
+    #   }
+    # }
+    # ingress_rule {
+    #   hostname = cloudflare_record.ssh-dev2_tuana9a_com.hostname
+    #   service  = "ssh://192.168.56.209:22"
+    #   origin_request {
+    #     bastion_mode             = false
+    #     disable_chunked_encoding = false
+    #     http2_origin             = false
+    #     keep_alive_connections   = 0
+    #     no_happy_eyeballs        = false
+    #     no_tls_verify            = false
+    #     proxy_port               = 0
+    #   }
+    # }
+    # ingress_rule {
+    #   hostname = cloudflare_record.amon_tuana9a_com.hostname
+    #   service  = "http://192.168.56.206:8206"
+    #   origin_request {
+    #     bastion_mode             = false
+    #     disable_chunked_encoding = false
+    #     http2_origin             = false
+    #     keep_alive_connections   = 0
+    #     no_happy_eyeballs        = false
+    #     no_tls_verify            = false
+    #     proxy_port               = 0
+    #   }
+    # }
     ingress_rule {
       service = "http_status:503"
     }
