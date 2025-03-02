364b374250e9493ce82009e0556686bfa702d6d3
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-03-02 07:15:23 +0000

    refactor(cloudflare-auth): remove edit-pages auth symlink

--- 022-pages/terraform.tf
+++ 022-pages/terraform.tf
@@ -32,6 +32,14 @@ provider "vault" {
   skip_child_token = true
 }
 
+data "vault_kv_secret" "edit_pages" {
+  path = "kv/cloudflare/accounts/tuana9a/api-tokens/edit-pages"
+}
+
+locals {
+  cloudflare_account_id = data.vault_kv_secret.edit_pages.data.cloudflare_account_id
+}
+
 provider "cloudflare" {
   api_token = data.vault_kv_secret.edit_pages.data.cloudflare_api_token
 }
