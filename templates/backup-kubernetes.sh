22884ba776e810bae10abee0af466a8eed9c6f25
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-04-09 13:16:24 +0000

    refactor: explicit backup kubernetes cronjob

--- templates/backup-kubernetes.sh
+++ templates/backup-kubernetes.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-echo "=== $(date) ==="
+echo "===$(date)==="
 
 HOST_NAME="$(hostname)"
 CLOUDFLARE_ACCOUNT_ID="{{ KUBERNETES_BACKUP_CLOUDFLARE_ACCOUNT_ID }}"
 S3_ENDPOINT="https://$CLOUDFLARE_ACCOUNT_ID.r2.cloudflarestorage.com"
 BUCKET_NAME="{{ KUBERNETES_BACKUP_BUCKET_NAME }}"
+DISCORD_WEBHOOK="{{ KUBERNETES_BACKUP_DISCORD_WEBHOOK }}"
+
+ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
+ETCDCTL_CERT=/etc/kubernetes/pki/apiserver-etcd-client.crt
+ETCDCTL_KEY=/etc/kubernetes/pki/apiserver-etcd-client.key
+ETCDCTL_OPTS="--cacert=$ETCDCTL_CACERT --cert=$ETCDCTL_CERT --key=$ETCDCTL_KEY"
 
 datehour=$(date '+%Y%m%d%H')
 hourminutesecond=$(date '+%H%M%S')
@@ -17,8 +23,6 @@ DUMP_FILE="$datehour-k8s-cobi-$HOST_NAME-backup-kubernetes.tar.gz"
 S3_OBJECT_KEY=$DUMP_FILE
 WORKDIR=/tmp
 SECONDS=0 # for calc duration
-
-# DISCORD_WEBHOOK="{{ KUBERNETES_BACKUP_DISCORD_WEBHOOK }}"
 
 export AWS_ACCESS_KEY_ID="{{ KUBERNETES_BACKUP_AWS_ACCESS_KEY_ID }}"
 export AWS_SECRET_ACCESS_KEY="{{ KUBERNETES_BACKUP_AWS_SECRET_ACCESS_KEY }}"
@@ -41,35 +45,30 @@ upload() {
   /usr/local/bin/aws s3api --endpoint-url $S3_ENDPOINT put-object --bucket $BUCKET_NAME --key $KEY --body $FILEPATH
 }
 
-mkdir -p $WORKDIR
 cd $WORKDIR || exit 1
 
-ETCDCTL_CACERT=/etc/kubernetes/pki/etcd/ca.crt
-ETCDCTL_CERT=/etc/kubernetes/pki/apiserver-etcd-client.crt
-ETCDCTL_KEY=/etc/kubernetes/pki/apiserver-etcd-client.key
-ETCDCTL_OPTS="--cacert=$ETCDCTL_CACERT --cert=$ETCDCTL_CERT --key=$ETCDCTL_KEY"
-
-echo === Members ===
+echo "===Members==="
 ETCDCTL_API=3 sudo /usr/local/bin/etcdctl member list $ETCDCTL_OPTS
 
-echo === Dump $ETCD_SNAPSHOT ===
+echo "===Dump $ETCD_SNAPSHOT==="
 ETCDCTL_API=3 sudo /usr/local/bin/etcdctl snapshot save $ETCD_SNAPSHOT $ETCDCTL_OPTS
 
-echo === Status ===
+echo "===Status==="
 sudo /usr/local/bin/etcdutl --write-out=table snapshot status $ETCD_SNAPSHOT
 
-echo === Zip $DUMP_FILE ===
+echo "===Zip==="
+echo "$DUMP_FILE"
 sudo tar -czvf $DUMP_FILE $ETCD_SNAPSHOT /etc/kubernetes/pki /etc/kubernetes/manifests
 
-echo === Upload "$S3_OBJECT_KEY" "$WORKDIR/$DUMP_FILE" ===
+echo "===Upload==="
+echo "$WORKDIR/$DUMP_FILE -> $S3_OBJECT_KEY"
 upload "$S3_OBJECT_KEY" "$DUMP_FILE"
 
-echo === Cleanup ===
-sudo rm -f snapshot.db
+echo "===Cleanup==="
+sudo rm -f *snapshot*.db
 sudo rm -f *backup-kubernetes*.tar.gz
 
-echo === Report ===
-
+echo "===Report==="
 DURATION=$SECONDS
 cat <<EOF | sudo kubectl --kubeconfig /etc/kubernetes/admin.conf apply -f -
 apiVersion: batch/v1
@@ -78,7 +77,7 @@ metadata:
   name: backup-kubernetes-reports-$datehour-$hourminutesecond-$HOST_NAME
   namespace: default
 spec:
-  ttlSecondsAfterFinished: 86400 # 1 days
+  ttlSecondsAfterFinished: 10800 # 3 hours
   template:
     spec:
       restartPolicy: Never
@@ -91,16 +90,15 @@ spec:
         - |
           push_gateway_baseurl="http://prometheus-prometheus-pushgateway.prometheus.svc.cluster.local:9091";
           POD_NAMESPACE=\$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace);
-          cat <<EOF | curl --data-binary @- \$push_gateway_baseurl/metrics/job/k8s_backup_cronjob
-          # TYPE k8s_backup_datehour gauge
+          echo '# TYPE k8s_backup_datehour gauge
           k8s_backup_datehour{namespace="\$POD_NAMESPACE"} $datehour
           # TYPE k8s_backup_duration gauge
           k8s_backup_duration{namespace="\$POD_NAMESPACE"} $DURATION
           # TYPE k8s_backup_unixtimestamp gauge
-          k8s_backup_unixtimestamp{namespace="\$POD_NAMESPACE"} $unixtimestamp
-          EOF
+          k8s_backup_unixtimestamp{namespace="\$POD_NAMESPACE"} $unixtimestamp' > body.txt
+          curl --data-binary @body.txt \$push_gateway_baseurl/metrics/job/k8s_backup_cronjob
 EOF
 
-echo === Notify ===
+echo "===Notify==="
 # MSG=":white_check_mark: \`backup-kubernetes\` \`$HOST_NAME\` \`$(($DURATION / 60))m$(($DURATION % 60))s\`"
 # notify "$MSG"
