4d3fbdba6b3324f3c6d276ebd53d40fdc7c2cbec
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-05-24 13:00:50 +0000

    chore: cleanup parkingx

--- 023-dns/dns_parkingx.tf
+++ 023-dns/dns_parkingx.tf
@@ -1,12 +1,12 @@
-data "vault_kv_secret" "parkingx_public_ip" {
-  path = "kv/public-ip/parkingx"
-}
+# data "vault_kv_secret" "parkingx_public_ip" {
+#   path = "kv/public-ip/parkingx"
+# }
 
-resource "cloudflare_record" "parkingx" {
-  zone_id = data.cloudflare_zone.tuana9a_com.id
-  name    = "parkingx"
-  content = data.vault_kv_secret.parkingx_public_ip.data.ip
-  type    = "A"
-  ttl     = 60
-  proxied = false
-}
+# resource "cloudflare_record" "parkingx" {
+#   zone_id = data.cloudflare_zone.tuana9a_com.id
+#   name    = "parkingx"
+#   content = data.vault_kv_secret.parkingx_public_ip.data.ip
+#   type    = "A"
+#   ttl     = 60
+#   proxied = false
+# }
