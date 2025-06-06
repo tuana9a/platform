9232a808c0284375a490351b8a622375fdfe1f12
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-06-06 03:07:59 +0000

    change(spegel): disable as breaking image pull always

--- 085-spegel/spegel.tf
+++ 085-spegel/spegel.tf
@@ -1,9 +1,9 @@
-resource "helm_release" "spegel" {
-  name             = "spegel"
-  namespace        = "spegel"
-  create_namespace = true
+# resource "helm_release" "spegel" {
+#   name             = "spegel"
+#   namespace        = "spegel"
+#   create_namespace = true
 
-  repository = "oci://ghcr.io/spegel-org/helm-charts"
-  chart      = "spegel"
-  version    = "v0.0.28"
-}
+#   repository = "oci://ghcr.io/spegel-org/helm-charts"
+#   chart      = "spegel"
+#   version    = "v0.0.28"
+# }
