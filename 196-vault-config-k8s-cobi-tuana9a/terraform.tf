249d3e60775fb8079e4acdb0766136fd8fed5cbc
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-06-20 23:49:08 +0000

    refactor: vault, external secrets, remove: vault secret operator

--- 196-vault-config-k8s-cobi-tuana9a/terraform.tf
+++ 196-vault-config-k8s-cobi-tuana9a/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket = "terraform-tuana9a"
-    prefix = "195-vault-setup-k8s-cobi-tuana9a"
+    prefix = "196-vault-config-k8s-cobi-tuana9a"
   }
   required_providers {
     google = {
