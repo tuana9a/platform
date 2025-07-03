5d677e70125b4650b8a6d93ccd421e1771b245ac
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-07-03 10:12:49 +0000

    add(cicd): dns

--- 196-vault-config-k8s-cobi-tuana9a/vault_policies.tf
+++ 196-vault-config-k8s-cobi-tuana9a/vault_policies.tf
@@ -44,3 +44,15 @@ path "sys/storage/raft/snapshot" {
 }
 EOT
 }
+
+resource "vault_policy" "dns_updater" {
+  name   = "dns-updater"
+  policy = <<EOT
+path "kv/cloudflare/accounts/tuana9a/api-tokens/edit-dns" {
+  capabilities = ["read"]
+}
+path "kv/public-ip/*" {
+  capabilities = ["read"]
+}
+EOT
+}
