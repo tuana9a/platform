d1d5ecab6e3af4be961475060e84acc4360cb1ec
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-07-28 12:25:31 +0000

    refactor(jenkins): rename to timestamper

--- 093-jenkins-JCasC/main.tf
+++ 093-jenkins-JCasC/main.tf
@@ -1,4 +1,7 @@
 resource "kubernetes_manifest" "manifests" {
   for_each = fileset(path.module, "manifests/*.yml")
   manifest = yamldecode(file(each.value))
+  field_manager {
+    force_conflicts = true
+  }
 }
