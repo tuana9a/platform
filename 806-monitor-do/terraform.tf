769eec249d3389ae4490f28f7f920cfd04c41943
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-08-12 23:23:42 +0000

    refactor(monitor-do): using direct do_token

--- 806-monitor-do/terraform.tf
+++ 806-monitor-do/terraform.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "hashicorp/google"
       version = "6.42.0"
     }
-    vault = {
-      source  = "hashicorp/vault"
-      version = "5.0.0"
-    }
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
@@ -25,16 +21,6 @@ provider "google" {
   zone    = "asia-southeast1-b"
 }
 
-provider "vault" {
-  address = "https://vault.tuana9a.com"
-
-  skip_child_token = true
-}
-
-data "vault_kv_secret" "do_token" {
-  path = "kv/digitalocean/tuana9a/tokens/monitor-do"
-}
-
 provider "digitalocean" {
-  token = data.vault_kv_secret.do_token.data.token
+  token = var.do_token
 }
