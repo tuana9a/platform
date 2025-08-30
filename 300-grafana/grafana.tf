004a4bdf96f4c4b07784a95863250d27abbcad4d
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-08-30 04:39:21 +0000

    refactor+add(grafana): simplify grafana stack, add cicd

--- 300-grafana/grafana.tf
+++ 300-grafana/grafana.tf
@@ -1,7 +1,3 @@
-resource "kubernetes_manifest" "admin_login_secret" {
-  manifest = yamldecode(file("./manifests/admin-login-secret.yaml"))
-}
-
 resource "helm_release" "grafana" {
   name             = "grafana"
   namespace        = "grafana"
@@ -12,10 +8,4 @@ resource "helm_release" "grafana" {
   version    = "8.6.4"
 
   values = [file("./values.yaml")]
-
-  depends_on = [kubernetes_manifest.admin_login_secret]
-}
-
-resource "kubernetes_manifest" "datasources" {
-  manifest = yamldecode(file("./manifests/datasources.yaml"))
 }
