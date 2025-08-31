c55551a9d3652e36b536624b50d9fedbd36ad56c
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-08-31 14:08:02 +0000

    add(r2): add cicd

---
+++ 021-r2/vars.tf
@@ -0,0 +1,19 @@
+variable "cloudflare_account_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_access_key_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "aws_secret_access_key" {
+  type      = string
+  sensitive = true
+}
