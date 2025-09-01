3f21f71416d2a89ed998b5e4cd868f753cdb719b
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-01 23:19:54 +0000

    chore(proxmox-cloud-img): update ubuntu22, add debian12

--- 020-proxmox-cloud-img/main.tf
+++ 020-proxmox-cloud-img/main.tf
@@ -1,9 +1,20 @@
-resource "proxmox_virtual_environment_file" "jammy_iso" {
+resource "proxmox_virtual_environment_file" "ubuntu22" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "xenomorph"
 
   source_file {
-    path = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+    path = "https://cloud-images.ubuntu.com/jammy/20250725/jammy-server-cloudimg-amd64.img"
+  }
+}
+
+resource "proxmox_virtual_environment_file" "debian12" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "xenomorph"
+
+  source_file {
+    file_name = "debian-12-generic-amd64.img"
+    path = "https://cloud.debian.org/images/cloud/bookworm/20250814-2204/debian-12-generic-amd64-20250814-2204.tar.xz"
   }
 }
