fd65b8cbf541dea557b78eeff2a467f000f3a794
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-01 23:58:51 +0000

    add(k8s-cobi-tuana9a): imports to terraform

---
+++ 068-k8s-cobi-tuana9a/imports.tf
@@ -0,0 +1,24 @@
+import {
+  id = "xenomorph/122"
+  to = proxmox_virtual_environment_vm.cluster["122"]
+}
+
+import {
+  id = "xenomorph/126"
+  to = proxmox_virtual_environment_vm.cluster["126"]
+}
+
+import {
+  id = "xenomorph/128"
+  to = proxmox_virtual_environment_vm.cluster["128"]
+}
+
+import {
+  id = "xenomorph/129"
+  to = proxmox_virtual_environment_vm.cluster["129"]
+}
+
+import {
+  id = "xenomorph/130"
+  to = proxmox_virtual_environment_vm.cluster["130"]
+}
