3fa7897cd07febeaa9146734e8e1af7d86e0b216
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-03 01:22:40 +0000

    add(gitlab-secrets): add cicd

---
+++ 205-gitlab-secrets-tuana9a/vars.tf
@@ -0,0 +1,10 @@
+variable "gitlab_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "vault_token" {
+  type        = string
+  sensitive   = true
+  description = "secret-operator vault token"
+}
