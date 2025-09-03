a438c6437eaaeb0f9fa5878c6b33dc470be214f9
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-03 01:50:01 +0000

    chores: terraform fmt

--- 020-proxmox-cloud-img/main.tf
+++ 020-proxmox-cloud-img/main.tf
@@ -15,6 +15,6 @@ resource "proxmox_virtual_environment_file" "debian12" {
 
   source_file {
     file_name = "debian-12-generic-amd64.img"
-    path = "https://cloud.debian.org/images/cloud/bookworm/20250814-2204/debian-12-generic-amd64-20250814-2204.tar.xz"
+    path      = "https://cloud.debian.org/images/cloud/bookworm/20250814-2204/debian-12-generic-amd64-20250814-2204.tar.xz"
   }
 }
---
+++ scripts/terraform_fmt_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+for f in $(find .  -name terraform.tf -not -path './.git*' -not -path './coder/templates/*' -not -path '*tmp*'); do
+  dir="$(dirname $f)"
+  echo "$dir"
+  cd $dir
+  terraform fmt
+  cd ..
+done
