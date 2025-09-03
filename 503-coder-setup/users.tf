4b335900369a171724eb9a0c1d1cf2a28ceaae73
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-03 01:27:21 +0000

    change(coder-setup): amon login with google

--- 503-coder-setup/users.tf
+++ 503-coder-setup/users.tf
@@ -1,19 +1,8 @@
-resource "random_password" "amon" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
-}
-
 resource "coderd_user" "amon" {
   username   = "adrazaamon"
   email      = "adrazaamon@gmail.com"
   name       = "Amon Ad-Raza"
-  login_type = "password"
-  password   = random_password.amon.result
-
-  lifecycle {
-    ignore_changes = [password]
-  }
+  login_type = "oidc"
 }
 
 resource "coderd_user" "long7" {
