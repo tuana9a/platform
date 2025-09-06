ea2c2e6e40c58d0e835268befcde685a63a92839
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-06 02:37:11 +0000

    change(github-runners): disable legacy actions runner controller

--- 095-github-runners/actions_runner_controller.tf
+++ 095-github-runners/actions_runner_controller.tf
@@ -1,21 +1,21 @@
-# LEGACY: https://github.com/actions/actions-runner-controller/blob/master/docs/quickstart.md
-resource "helm_release" "actions_runner_controller" {
-  name             = "actions-runner-controller"
-  namespace        = "github-runners"
-  create_namespace = true
-  repository       = "https://actions-runner-controller.github.io/actions-runner-controller"
-  chart            = "actions-runner-controller"
-  version          = "0.23.7"
+# # LEGACY: https://github.com/actions/actions-runner-controller/blob/master/docs/quickstart.md
+# resource "helm_release" "actions_runner_controller" {
+#   name             = "actions-runner-controller"
+#   namespace        = "github-runners"
+#   create_namespace = true
+#   repository       = "https://actions-runner-controller.github.io/actions-runner-controller"
+#   chart            = "actions-runner-controller"
+#   version          = "0.23.7"
 
-  set {
-    name  = "authSecret.create"
-    value = "true"
-    type  = "string"
-  }
+#   set {
+#     name  = "authSecret.create"
+#     value = "true"
+#     type  = "string"
+#   }
 
-  set_sensitive {
-    name  = "authSecret.github_token"
-    value = var.github_token
-    type  = "string"
-  }
-}
+#   set_sensitive {
+#     name  = "authSecret.github_token"
+#     value = var.github_token
+#     type  = "string"
+#   }
+# }
