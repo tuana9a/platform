053727c595b51e03ec14b7d43d320f93e52f770b
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-08 12:35:01 +0000

    change(spegel): disable spegel
    unstable, feel worried

--- 085-spegel/spegel.tf
+++ 085-spegel/spegel.tf
@@ -1,9 +1,9 @@
-resource "helm_release" "spegel" {
-  name             = "spegel"
-  namespace        = "spegel"
-  create_namespace = true
+# resource "helm_release" "spegel" {
+#   name             = "spegel"
+#   namespace        = "spegel"
+#   create_namespace = true
 
-  repository = "oci://ghcr.io/spegel-org/helm-charts"
-  chart      = "spegel"
-  version    = "0.3.0"
-}
+#   repository = "oci://ghcr.io/spegel-org/helm-charts"
+#   chart      = "spegel"
+#   version    = "0.3.0"
+# }
