c5f1e95a366fb18bc27f978769741862a4c855ac
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-08 12:27:47 +0000

    chores(proxmox-img): debian12 qcow2

--- 020-proxmox-cloud-img/main.tf
+++ 020-proxmox-cloud-img/main.tf
@@ -1,20 +1,21 @@
-resource "proxmox_virtual_environment_file" "ubuntu22" {
+resource "proxmox_virtual_environment_file" "ubuntu22-20250725" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "xenomorph"
 
   source_file {
-    path = "https://cloud-images.ubuntu.com/jammy/20250725/jammy-server-cloudimg-amd64.img"
+    file_name = "jammy-server-cloudimg-amd64.img"
+    path      = "https://cloud-images.ubuntu.com/jammy/20250725/jammy-server-cloudimg-amd64.img"
   }
 }
 
-resource "proxmox_virtual_environment_file" "debian12" {
+resource "proxmox_virtual_environment_file" "debian12-20250814-2204" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "xenomorph"
 
   source_file {
-    file_name = "debian-12-generic-amd64.img"
-    path      = "https://cloud.debian.org/images/cloud/bookworm/20250814-2204/debian-12-generic-amd64-20250814-2204.tar.xz"
+    file_name = "debian-12-generic-amd64-20250814-2204.img"
+    path      = "https://cloud.debian.org/images/cloud/bookworm/20250814-2204/debian-12-generic-amd64-20250814-2204.qcow2"
   }
 }
