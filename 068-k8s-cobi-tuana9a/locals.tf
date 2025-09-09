87152924623238e1043815f4e898c0ef4d4f40a9
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-09 15:17:36 +0000

    change(k8s-cobi): add i-132, remove i-128,i-129

--- 068-k8s-cobi-tuana9a/locals.tf
+++ 068-k8s-cobi-tuana9a/locals.tf
@@ -18,22 +18,22 @@ locals {
 
       template_vm_id = 1002
     }
-    128 = {
-      vmid      = 128
-      vmip      = "192.168.56.28"
-      corecount = 4
-      memsize   = 8192
+    # 128 = {
+    #   vmid      = 128
+    #   vmip      = "192.168.56.28"
+    #   corecount = 4
+    #   memsize   = 8192
 
-      template_vm_id = 1002
-    }
-    129 = {
-      vmid      = 129
-      vmip      = "192.168.56.29"
-      corecount = 4
-      memsize   = 8192
+    #   template_vm_id = 1002
+    # }
+    # 129 = {
+    #   vmid      = 129
+    #   vmip      = "192.168.56.29"
+    #   corecount = 4
+    #   memsize   = 8192
 
-      template_vm_id = 1002
-    }
+    #   template_vm_id = 1002
+    # }
     130 = {
       vmid      = 130
       vmip      = "192.168.56.30"
@@ -50,6 +50,15 @@ locals {
       memsize        = 8192
       vmip           = "192.168.56.31"
       address        = "192.168.56.31/24"
+      gateway_ip     = "192.168.56.1"
+      network_device = "vmbr56"
+    }
+    132 = {
+      vmid           = 132
+      corecount      = 4
+      memsize        = 8192
+      vmip           = "192.168.56.32"
+      address        = "192.168.56.32/24"
       gateway_ip     = "192.168.56.1"
       network_device = "vmbr56"
     }
