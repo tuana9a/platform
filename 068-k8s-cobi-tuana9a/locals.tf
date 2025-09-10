eb8e20deb0eed198e8cda30d87cfd759be5b8d69
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-10 13:47:37 +0000

    change(k8s-cobi): 100% migrated from tuana9a/kp

--- 068-k8s-cobi-tuana9a/locals.tf
+++ 068-k8s-cobi-tuana9a/locals.tf
@@ -1,49 +1,32 @@
 locals {
-  template_vm_id = 1002
-  vm_list_imported = {
-    122 = {
-      vmid      = 122
-      vmip      = "192.168.56.22"
-      corecount = 2
-      memsize   = 4096
-      disksize  = 20
-
-      template_vm_id = 1002
-    }
-    # 126 = {
-    #   vmid      = 126
-    #   vmip      = "192.168.56.26"
-    #   corecount = 4
-    #   memsize   = 8192
-
-    #   template_vm_id = 1002
-    # }
-    # 128 = {
-    #   vmid      = 128
-    #   vmip      = "192.168.56.28"
-    #   corecount = 4
-    #   memsize   = 8192
-
-    #   template_vm_id = 1002
-    # }
-    # 129 = {
-    #   vmid      = 129
-    #   vmip      = "192.168.56.29"
-    #   corecount = 4
-    #   memsize   = 8192
-
-    #   template_vm_id = 1002
-    # }
-    # 130 = {
-    #   vmid      = 130
-    #   vmip      = "192.168.56.30"
-    #   corecount = 4
-    #   memsize   = 8192
-
-    #   template_vm_id = 1002
-    # }
-  }
   vm_list = {
+    123 = {
+      vmid           = 123
+      corecount      = 2
+      memsize        = 4096
+      vmip           = "192.168.56.23"
+      address        = "192.168.56.23/24"
+      gateway_ip     = "192.168.56.1"
+      network_device = "vmbr56"
+    }
+    124 = {
+      vmid           = 124
+      corecount      = 2
+      memsize        = 4096
+      vmip           = "192.168.56.24"
+      address        = "192.168.56.24/24"
+      gateway_ip     = "192.168.56.1"
+      network_device = "vmbr56"
+    }
+    125 = {
+      vmid           = 125
+      corecount      = 2
+      memsize        = 4096
+      vmip           = "192.168.56.25"
+      address        = "192.168.56.25/24"
+      gateway_ip     = "192.168.56.1"
+      network_device = "vmbr56"
+    }
     131 = {
       vmid           = 131
       corecount      = 4
