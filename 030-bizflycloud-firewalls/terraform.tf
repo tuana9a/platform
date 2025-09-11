01d709cd20775183245fb38868d000d45bc5842c
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-11 22:31:52 +0000

    add(bizflycloud-firewalls): add cicd

--- 030-bizflycloud-firewalls/terraform.tf
+++ 030-bizflycloud-firewalls/terraform.tf
@@ -8,10 +8,6 @@ terraform {
       source  = "hashicorp/google"
       version = "5.29.1"
     }
-    vault = {
-      source  = "hashicorp/vault"
-      version = "4.4.0"
-    }
     bizflycloud = {
       source  = "bizflycloud/bizflycloud"
       version = "0.1.7"
@@ -25,19 +21,9 @@ provider "google" {
   zone    = "asia-southeast1-b"
 }
 
-provider "vault" {
-  address = "https://vault.tuana9a.com"
-
-  skip_child_token = true
-}
-
-data "vault_kv_secret" "terraform" {
-  path = "kv/bizflycloud/accounts/tuana9a/api-tokens/terraform"
-}
-
 provider "bizflycloud" {
   auth_method                   = "application_credential"
   region_name                   = "HaNoi"
-  application_credential_id     = data.vault_kv_secret.terraform.data.bizflycloud_application_credential_id
-  application_credential_secret = data.vault_kv_secret.terraform.data.bizflycloud_application_credential_secret
+  application_credential_id     = var.bizflycloud_application_credential_id
+  application_credential_secret = var.bizflycloud_application_credential_secret
 }
---
+++ 030-bizflycloud-firewalls/vars.tf
@@ -0,0 +1,9 @@
+variable "bizflycloud_application_credential_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "bizflycloud_application_credential_secret" {
+  type      = string
+  sensitive = true
+}
