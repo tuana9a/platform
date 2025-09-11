c76ea2f244c7b3f362e1baffe0e72b5e477b08b2
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-11 15:33:41 +0000

    add(platform): add facehugger, add orisis
    orisis as public server and will forward request to facehugger
    facehugger running inside proxmox receive request from orisis then forward into k8s cluster

--- scripts/get_ansible_password.sh
+++ scripts/get_ansible_password.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s --header "X-Vault-Token: $(cat ~/.vault-token)" --request GET https://vault.tuana9a.com/v1/kv/platform/ansible-vault | jq -r '.data.password'
+curl -s --header "X-Vault-Token: $(cat ~/.vault-token)" --request GET https://vault.tuana9a.com/v1/kv/ansible-vault-password | jq -r '.data.password'
