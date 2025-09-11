901df5c722c23db572a71c85a685e19bb1fffcc6
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-11 23:46:38 +0000

    chores(dns,k9s): terraform fmt, upgrade k9s v0.50.9

--- 023-dns/locals.tf
+++ 023-dns/locals.tf
@@ -1,20 +1,20 @@
 locals {
   servers = yamldecode(file(var.servers_file))
   apps = {
-    "argocd"           = local.servers.orisis.ip
-    "*.coder"          = local.servers.orisis.ip
-    "coder"            = local.servers.orisis.ip
-    "d9stbot"          = local.servers.orisis.ip
-    "*.dev2"           = local.servers.orisis.ip
-    "dev2"             = local.servers.orisis.ip
-    "dkhptd-api"       = local.servers.orisis.ip
-    "grafana"          = local.servers.orisis.ip
-    "hcr"              = local.servers.orisis.ip
-    "jenkins"          = local.servers.orisis.ip
-    "paste"            = local.servers.orisis.ip
-    "t9stbot"          = local.servers.orisis.ip
-    "uuid.stuffs.kn"   = local.servers.orisis.ip
-    "vault"            = local.servers.orisis.ip
+    "argocd"         = local.servers.orisis.ip
+    "*.coder"        = local.servers.orisis.ip
+    "coder"          = local.servers.orisis.ip
+    "d9stbot"        = local.servers.orisis.ip
+    "*.dev2"         = local.servers.orisis.ip
+    "dev2"           = local.servers.orisis.ip
+    "dkhptd-api"     = local.servers.orisis.ip
+    "grafana"        = local.servers.orisis.ip
+    "hcr"            = local.servers.orisis.ip
+    "jenkins"        = local.servers.orisis.ip
+    "paste"          = local.servers.orisis.ip
+    "t9stbot"        = local.servers.orisis.ip
+    "uuid.stuffs.kn" = local.servers.orisis.ip
+    "vault"          = local.servers.orisis.ip
   }
   mx_cloudflare_routes = [
     {
