24cb8db2d8c7557eb084702dc6eabbab4be43ca8
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-09-14 04:27:12 +0000

    add(cloudimg-neomorph): add cloudimg neomorph

---
+++ 020-proxmox-cloudimg-neomorph/terraform.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-tuana9a"
+    prefix = "020-proxmox-cloudimg-neomorph"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.29.1"
+    }
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.79.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "tuana9a"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-b"
+}
+
+provider "proxmox" {
+  endpoint  = var.pve_endpoint
+  api_token = var.pve_api_token
+  insecure  = var.pve_insecure
+}
