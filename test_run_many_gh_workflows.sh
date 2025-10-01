9669409de576e9968f2e9fba2bf1ed801d1f5ab1
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-10-01 01:30:49 +0000

    refactor: better test run many gh workflow script

--- test_run_many_gh_workflows.sh
+++ test_run_many_gh_workflows.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+ref="$(git branch --show-current)"
+
+while getopts "r:" opt; do
+  case $opt in
+    r) ref="$OPTARG"; ;;
+    ?) exit 1 ;;
+  esac
+done
+
+echo ref=$ref
+
 for x in $(find '.github/workflows' -type f); do
-    if [[ $(grep -q "workflow_dispatch" $x) -eq 0 ]]; then
-        gh workflow run $(basename $x) &
-    fi
+  grep -q "workflow_dispatch" $x
+  has_dispatch=$?
+  grep -q "inputs" $x
+  has_inputs=$?
+  # echo $x $has_dispatch $has_inputs
+  if [[ $has_dispatch -eq 0 ]] && [[ $has_inputs -ne 0 ]]; then
+    echo gh workflow run --ref $ref $(basename $x)
+    gh workflow run --ref $ref $(basename $x) &
+  fi
 done
