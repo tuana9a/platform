f48ed25811ae93a3b9015d9da686498ed978c5d2
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-10-03 13:46:24 +0000

    fix: scrape prometheus-pushgateway

--- 300-prometheus-pushgateway-k8s-cobi/prometheus_pushgateway.tf
+++ 300-prometheus-pushgateway-k8s-cobi/prometheus_pushgateway.tf
@@ -6,4 +6,6 @@ resource "helm_release" "prometheus_pushgateway" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus-pushgateway"
   version    = "2.12.0"
+
+  values = [file("./values.yaml")]
 }
