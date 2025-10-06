9759a760b81dac4257746c8bde6a0e275f0be498
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-10-06 13:44:55 +0000

    add(aws): t9st aws account alias

---
+++ 001-aws-account-alias-t9st/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-tuana9a"
+    prefix = "001-aws-account-alias-t9st"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.29.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "google" {
+  project = "tuana9a"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-b"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
