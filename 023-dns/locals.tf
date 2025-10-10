a3631028304ceb96602e2b075e5ba76335049ee3
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-10-10 23:38:25 +0000

    chore: terraform fmt all

--- 023-dns/locals.tf
+++ 023-dns/locals.tf
@@ -1,23 +1,23 @@
 locals {
   servers = yamldecode(file(var.servers_file))
   apps = {
-    "argocd"         = local.servers.orisis.ip
-    "*.coder"        = local.servers.orisis.ip
-    "coder"          = local.servers.orisis.ip
-    "d9stbot"        = local.servers.orisis.ip
-    "*.dev2"         = local.servers.orisis.ip
-    "dev2"           = local.servers.orisis.ip
-    "*.jnt724-dev"   = local.servers.orisis.ip
-    "jnt724-dev"     = local.servers.orisis.ip
-    "dkhptd-api"     = local.servers.orisis.ip
-    "grafana"        = local.servers.orisis.ip
-    "hcr"            = local.servers.orisis.ip
-    "jenkins"        = local.servers.orisis.ip
-    "paste"          = local.servers.orisis.ip
-    "t9stbot"        = local.servers.orisis.ip
-    "*.stuffs.kn"    = local.servers.orisis.ip # knative serverless
-    "vault"          = local.servers.orisis.ip
-    "neomorph"       = local.servers.orisis.ip
+    "argocd"       = local.servers.orisis.ip
+    "*.coder"      = local.servers.orisis.ip
+    "coder"        = local.servers.orisis.ip
+    "d9stbot"      = local.servers.orisis.ip
+    "*.dev2"       = local.servers.orisis.ip
+    "dev2"         = local.servers.orisis.ip
+    "*.jnt724-dev" = local.servers.orisis.ip
+    "jnt724-dev"   = local.servers.orisis.ip
+    "dkhptd-api"   = local.servers.orisis.ip
+    "grafana"      = local.servers.orisis.ip
+    "hcr"          = local.servers.orisis.ip
+    "jenkins"      = local.servers.orisis.ip
+    "paste"        = local.servers.orisis.ip
+    "t9stbot"      = local.servers.orisis.ip
+    "*.stuffs.kn"  = local.servers.orisis.ip # knative serverless
+    "vault"        = local.servers.orisis.ip
+    "neomorph"     = local.servers.orisis.ip
   }
   mx_cloudflare_routes = [
     {
