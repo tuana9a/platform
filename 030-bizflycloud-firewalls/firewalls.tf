e6963a697b997288adef61a02cd5bcdbc8624620
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-11-10 15:33:08 +0000

    refactor(k8s-cluster): orisis forward k8s workers directly - add wireguard_heartbeat for retry connect to wireguard server - add: enable/disable haproxy stats for debug - add: bizflycloud firewall allow 9000 haproxy stats - add: vscode task for convenient - ansible inventory code refactor remove k8s_cluster_vars

--- 030-bizflycloud-firewalls/firewalls.tf
+++ 030-bizflycloud-firewalls/firewalls.tf
@@ -127,3 +127,20 @@ resource "bizflycloud_firewall" "allow_node_exporter" {
     local.servers.orisis.network_interfaces.0.id,
   ]
 }
+
+resource "bizflycloud_firewall" "allow_haproxy_stats" {
+  name = "allow_haproxy_stats"
+  ingress {
+    cidr       = "14.0.0.0/8"
+    port_range = "9000"
+    protocol   = "tcp"
+  }
+  egress {
+    cidr       = ""
+    port_range = ""
+    protocol   = ""
+  }
+  network_interfaces = [
+    local.servers.orisis.network_interfaces.0.id,
+  ]
+}
