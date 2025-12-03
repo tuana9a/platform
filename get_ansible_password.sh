69582b6afa7e92c2865e00224d98abda514481ef
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-12-03 02:36:56 +0000

    refactor(ansible): using static ansible password file locally

--- get_ansible_password.sh
+++ get_ansible_password.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s --header "X-Vault-Token: $(cat ~/.vault-token)" --request GET https://vault.tuana9a.com/v1/kv/ansible/vault | jq -r '.data.password'
+curl -sSf --header "X-Vault-Token: $(cat ~/.vault-token)" --request GET https://vault.tuana9a.com/v1/kv/ansible/vault | jq -r '.data.password'
