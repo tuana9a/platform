2b6377f5114f338be1f74b523cceca8be81b3c72
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-12-22 07:20:03 +0000

    change(coder): temporary uninstall coder

--- 502-coder/coder.tf
+++ 502-coder/coder.tf
@@ -2,16 +2,16 @@ resource "kubernetes_manifest" "coder_env" {
   manifest = yamldecode(file("manifests/coder-env.yaml"))
 }
 
-resource "helm_release" "coder" {
-  name             = "coder"
-  namespace        = "coder"
-  create_namespace = true
+# resource "helm_release" "coder" {
+#   name             = "coder"
+#   namespace        = "coder"
+#   create_namespace = true
 
-  repository = "https://helm.coder.com/v2"
-  chart      = "coder"
-  version    = "2.23.1"
+#   repository = "https://helm.coder.com/v2"
+#   chart      = "coder"
+#   version    = "2.23.1"
 
-  values = [file("./values.yaml")]
+#   values = [file("./values.yaml")]
 
-  depends_on = [kubernetes_manifest.coder_env]
-}
+#   depends_on = [kubernetes_manifest.coder_env]
+# }
