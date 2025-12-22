582fb485f03d1e60b8eb4d6e111cc99bab945710
Nguyen Minh Tuan <tuana9a@gmail.com>
2025-12-22 12:13:51 +0000

    change(coder): allow home disk size 5GB - 100GB, min memory 2GB

--- 503-coder-setup/templates/statefulset/parameter.tf
+++ 503-coder-setup/templates/statefulset/parameter.tf
@@ -2,9 +2,9 @@ data "coder_parameter" "cpu" {
   name         = "cpu"
   display_name = "CPU"
   description  = "The number of CPU cores"
-  default      = "1"
   icon         = "/icon/memory.svg"
   mutable      = true
+  default      = "1"
   option {
     name  = "1 Cores"
     value = "1"
@@ -23,13 +23,9 @@ data "coder_parameter" "memory" {
   name         = "memory"
   display_name = "Memory"
   description  = "The amount of memory in GB"
-  default      = "1"
   icon         = "/icon/memory.svg"
   mutable      = true
-  option {
-    name  = "1 GB"
-    value = "1"
-  }
+  default      = "2"
   option {
     name  = "2 GB"
     value = "2"
@@ -50,8 +46,8 @@ data "coder_parameter" "memory" {
 
 data "coder_parameter" "home_persistent" {
   name         = "home_persistent"
-  display_name = "home_persistent"
-  description  = "home dir persistent"
+  display_name = "Home dir persistent"
+  description  = "Whether home dir is persistent or ephemeral"
   mutable      = true
   type         = "string"
   default      = "no"
@@ -69,19 +65,19 @@ data "coder_parameter" "home_disk_size" {
   name         = "home_disk_size"
   display_name = "Home disk size"
   description  = "The size of the home disk in GB"
-  default      = "2"
   type         = "number"
   icon         = "/emojis/1f4be.png"
   mutable      = false
+  default      = "10"
   validation {
-    min = 1
-    max = 10
+    min = 5
+    max = 100
   }
 }
 
 data "coder_parameter" "home_storage_class" {
   name         = "home_storage_class"
-  display_name = "home storage class"
+  display_name = "Home storage class"
   description  = "home storage class"
   mutable      = false
   type         = "string"
@@ -95,7 +91,7 @@ data "coder_parameter" "home_storage_class" {
 data "coder_parameter" "image_tag" {
   name         = "image_tag"
   display_name = "Image tag"
-  description  = "tuana9a/coder image tag"
+  description  = "Docker image tuana9a/coder tag"
   default      = "base"
   icon         = "/icon/docker.png"
   mutable      = true
