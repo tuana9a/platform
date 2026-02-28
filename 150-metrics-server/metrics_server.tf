a03f6769d20d2d576521bc8e37fb1990357df44d
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-02-28 07:39:01 +0000

    change(metrics-server): move to namespace kube-system and fixing cicd

--- 150-metrics-server/metrics_server.tf
+++ 150-metrics-server/metrics_server.tf
@@ -1,7 +1,6 @@
 resource "helm_release" "metrics_server" {
   name             = "metrics-server"
-  namespace        = "metrics-server"
-  create_namespace = true
+  namespace        = "kube-system"
 
   repository = "https://kubernetes-sigs.github.io/metrics-server"
   chart      = "metrics-server"
