fc2d8954d16bb8ecc95b2c718c9781e8f1e01d6f
Tuan Nguyen <tuana91a@gmail.com>
2026-03-08 14:35:10 +0700

    change(terraform): terraform k8s token auth (#145)
    * change(external-dns): terraform k8s token auth
    
    * change(ingress-kong): terraform k8s token auth
    
    * change(ingress-nginx): terraform k8s token auth
    
    * change(grafana): terraform k8s token auth
    
    * change(loki): terraform k8s token auth
    
    * change(prometheus): terraform k8s token auth
    
    * change(promtail): terraform k8s token auth
    
    * change(coder): terraform k8s token auth
    
    * change(k8s-secrets): terraform k8s token auth

--- 200-ingress-nginx/vars.tf
+++ 200-ingress-nginx/vars.tf
@@ -1,4 +1,10 @@
-variable "kubeconfig" {
-  type        = string
-  description = "path to kubeconfig"
+variable "cluster_ca_certificate_b64" {
+  type      = string
+  sensitive = true
+}
+
+# k8s service account token
+variable "token" {
+  type      = string
+  sensitive = true
 }
--- k8s-secrets/vars.tf
+++ k8s-secrets/vars.tf
@@ -2,3 +2,14 @@ variable "all_secrets_file" {
   type      = string
   sensitive = false
 }
+
+variable "cluster_ca_certificate_b64" {
+  type      = string
+  sensitive = true
+}
+
+# k8s service account token
+variable "token" {
+  type      = string
+  sensitive = true
+}
