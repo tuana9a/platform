a6bd4f4dc67a93ab955f99d215e7ab7579d9e6e3
tuana91a <tuana91a@gmail.com>
2026-03-08 13:22:45 +0700

    change(github-runners): terraform k8s token auth

--- 095-github-runners/vars.tf
+++ 095-github-runners/vars.tf
@@ -8,3 +8,13 @@ variable "github_token" {
   type      = string
   sensitive = true
 }
+
+variable "cluster_ca_certificate_b64" {
+  type      = string
+  sensitive = true
+}
+
+variable "token" {
+  type      = string
+  sensitive = true
+}
