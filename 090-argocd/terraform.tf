bdef67b012206708c082ab72880beacdc12c9144
tuana91a <tuana91a@gmail.com>
2026-03-08 13:13:45 +0700

    chore(argocd): tf code format

--- 090-argocd/terraform.tf
+++ 090-argocd/terraform.tf
@@ -25,6 +25,6 @@ provider "helm" {
   kubernetes {
     host                   = "https://192.168.56.21:6443"
     cluster_ca_certificate = base64decode(var.cluster_ca_certificate_b64)
-    token = var.token
+    token                  = var.token
   }
 }
