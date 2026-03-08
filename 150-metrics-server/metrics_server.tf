441cc8aae846db6c1406e0506ec95a30cb6ce726
tuana91a <tuana91a@gmail.com>
2026-03-08 13:24:43 +0700

    change(metrics-server): terraform k8s token auth

--- 150-metrics-server/metrics_server.tf
+++ 150-metrics-server/metrics_server.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "metrics_server" {
-  name             = "metrics-server"
-  namespace        = "kube-system"
+  name      = "metrics-server"
+  namespace = "kube-system"
 
   repository = "https://kubernetes-sigs.github.io/metrics-server"
   chart      = "metrics-server"
