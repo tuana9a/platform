e881fe147ae46f0e78552fece91ba85d42dd7a6e
tuana91a <tuana91a@gmail.com>
2026-03-08 13:54:29 +0700

    change(external-dns): terraform k8s token auth

--- 200-external-dns/terraform.tf
+++ 200-external-dns/terraform.tf
@@ -23,5 +23,9 @@ provider "google" {
 
 provider "helm" {
   kubernetes {
+    host                   = "https://192.168.56.21:6443"
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate_b64)
+
+    token = var.token
   }
 }
