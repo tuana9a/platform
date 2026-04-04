17b6c56c9fee5a3238680f64fc610cce2ef05982
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-04-04 07:46:55 +0000

    change(k8s-cobi): disable kured for stable running cluster

--- 086-kured-k8s-cobi/kured.tf
+++ 086-kured-k8s-cobi/kured.tf
@@ -1,9 +1,9 @@
-resource "helm_release" "kured" {
-  name             = "kured"
-  namespace        = "kured"
-  create_namespace = true
+# resource "helm_release" "kured" {
+#   name             = "kured"
+#   namespace        = "kured"
+#   create_namespace = true
 
-  repository = "oci://ghcr.io/kubereboot/charts"
-  chart      = "kured"
-  version    = "5.10.0"
-}
+#   repository = "oci://ghcr.io/kubereboot/charts"
+#   chart      = "kured"
+#   version    = "5.10.0"
+# }
