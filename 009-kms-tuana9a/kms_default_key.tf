cc85f9f4521bc4e01b8dc9fd99d092e73c88ba74
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-04-06 11:40:40 +0000

    add: kms-tuana9a

---
+++ 009-kms-tuana9a/kms_default_key.tf
@@ -0,0 +1,65 @@
+resource "aws_kms_key" "default_key" {
+  description = "A default key to encrypt decrypt anything" # sops for now
+
+  enable_key_rotation     = true # TODO: what is this
+  deletion_window_in_days = 20   # TODO: what is this
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "platform-secrets"
+    Statement = [
+      {
+        # TODO: why this is required
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow administration of the key"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/terraform-kms-admin"
+        },
+        Action = [
+          "kms:ReplicateKey",
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion"
+        ],
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow use of the key"
+        Effect = "Allow"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-workflow",
+            "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/ap-southeast-1/AWSReservedSSO_Admin_c31bf84842b4b2b9",
+          ]
+        },
+        Action = [
+          "kms:DescribeKey",
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey",
+          "kms:GenerateDataKeyWithoutPlaintext"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
---
+++ 009-kms-tuana9a/outputs.tf
@@ -0,0 +1,3 @@
+output "default_key_arn" {
+  value = aws_kms_key.default_key.arn
+}
