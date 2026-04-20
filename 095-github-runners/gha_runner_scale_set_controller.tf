0f50d7dfdf815f2f121d9d02405d34b4b4927911
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-04-20 12:26:06 +0000

    refactor(github-runners): add prerequisite secret

--- 095-github-runners/gha_runner_scale_set_controller.tf
+++ 095-github-runners/gha_runner_scale_set_controller.tf
@@ -1,11 +1,32 @@
+import {
+  to = kubernetes_namespace_v1.github_runners
+  id = "github-runners"
+}
+
+resource "kubernetes_namespace_v1" "github_runners" {
+  metadata {
+    name = "github-runners"
+  }
+}
+
+resource "kubernetes_secret_v1" "pre_defined_secret" {
+  metadata {
+    name      = "pre-defined-secret"
+    namespace = kubernetes_namespace_v1.github_runners.metadata[0].name
+  }
+
+  data = {
+    github_token = local.secrets.github_token
+  }
+}
+
 # NEW: https://docs.github.com/en/actions/tutorials/use-actions-runner-controller/quickstart
 resource "helm_release" "gha_runner_scale_set_controller" {
-  name             = "arc"
-  namespace        = "github-runners"
-  create_namespace = true
-  repository       = "oci://ghcr.io/actions/actions-runner-controller-charts"
-  chart            = "gha-runner-scale-set-controller"
-  version          = "0.12.1"
+  name       = "arc"
+  namespace  = kubernetes_namespace_v1.github_runners.metadata[0].name
+  repository = "oci://ghcr.io/actions/actions-runner-controller-charts"
+  chart      = "gha-runner-scale-set-controller"
+  version    = "0.12.1"
 
   set {
     # https://docs.github.com/en/actions/tutorials/use-actions-runner-controller/authenticate-to-the-api#authenticating-arc-with-a-personal-access-token-classic
