c208066f014d5cac83cd3f496e730c364ff2bff9
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-04-20 11:36:19 +0000

    refactor(jenkins): include prerequisite google-login secret

--- 092-jenkins/jenkins.tf
+++ 092-jenkins/jenkins.tf
@@ -1,7 +1,31 @@
+import {
+  to = kubernetes_namespace_v1.jenkins
+  id = "jenkins"
+}
+
+resource "kubernetes_namespace_v1" "jenkins" {
+  metadata {
+    name = "jenkins"
+  }
+}
+
+resource "kubernetes_secret_v1" "google_login" {
+  metadata {
+    name      = "google-login"
+    namespace = kubernetes_namespace_v1.jenkins.metadata[0].name
+  }
+
+  data = {
+    client-id     = local.secrets.google-login.client-id
+    client-secret = local.secrets.google-login.client-secret
+  }
+}
+
 resource "helm_release" "jenkins" {
-  name             = "jenkins"
-  namespace        = "jenkins"
-  create_namespace = true
+  depends_on = [kubernetes_secret_v1.google_login]
+
+  name      = "jenkins"
+  namespace = kubernetes_namespace_v1.jenkins.metadata[0].name
 
   repository = "https://charts.jenkins.io"
   chart      = "jenkins"
--- 092-jenkins/terraform.tf
+++ 092-jenkins/terraform.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/google"
       version = "5.29.1"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.1.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "2.12.1"
@@ -25,11 +29,18 @@ provider "google" {
   zone    = "asia-southeast1-b"
 }
 
+provider "kubernetes" {
+  host                   = "https://192.168.56.21:6443"
+  cluster_ca_certificate = base64decode(local.secrets.cluster_ca_certificate_b64)
+
+  token = local.secrets.cluster_auth_token
+}
+
 provider "helm" {
   kubernetes {
     host                   = "https://192.168.56.21:6443"
     cluster_ca_certificate = base64decode(local.secrets.cluster_ca_certificate_b64)
-    token                  = local.secrets.token
+    token                  = local.secrets.cluster_auth_token
   }
 }
 
