28a6e6edf64674111e6221686231428c6aa907df
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-04-24 01:11:01 +0000

    chore(sso): rename user to Zero Two

--- 003-aws-sso-iam/sso_users.tf
+++ 003-aws-sso-iam/sso_users.tf
@@ -1,12 +1,12 @@
 resource "aws_identitystore_user" "zero_tuana9a_com" {
   identity_store_id = local.sso_identity_store_id
 
-  display_name = "Aki"
+  display_name = "Zero Two"
   user_name    = "zero@tuana9a.com"
 
   name {
-    given_name  = "Aki"
-    family_name = "Ng"
+    given_name  = "Zero"
+    family_name = "Two"
   }
 
   emails {
