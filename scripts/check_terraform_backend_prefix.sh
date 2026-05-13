8f340a8eda8ca7759d2a38d0025bdc7aee039d60
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-05-13 13:49:17 +0000

    add: pre-commit, check_terraform_backend_prefix.sh, check_terraform_fmt.sh

--- scripts/check_terraform_backend_prefix.sh
+++ scripts/check_terraform_backend_prefix.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 delimiter=">"
 
-for f in $(find * -maxdepth 1 -name terraform.tf -not -path './.git*' -not -path '*tmp*'); do
+for f in $(find * -maxdepth 5 -name terraform.tf -not -path './.git*' -not -path '*tmp*'); do
   dir="$(dirname "$f")"
   key=$dir
   if grep --quiet 'prefix' "$f"; then
     echo -n "$f $delimiter $dir $delimiter "
     if ! grep -o "prefix = \"$key\"" "$f"; then
-      echo "ERROR"
+      echo
+      echo "ERROR: terraform backend prefix is not correct, it should be '$key'"
       exit 1
     fi
   fi
--- scripts/check_terraform_fmt.sh
+++ scripts/check_terraform_fmt.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 basedir=$(pwd)
 
-for f in $(find * -maxdepth 1 -name terraform.tf -not -path './.git*' -not -path '*tmp*'); do
+for f in $(find * -maxdepth 5 -name terraform.tf -not -path './.git*' -not -path '*tmp*'); do
   dir="$(dirname "$f")"
   echo "$dir"
   cd "$dir"
