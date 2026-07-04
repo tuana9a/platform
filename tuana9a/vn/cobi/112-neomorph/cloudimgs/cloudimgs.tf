ef5d0493c7e63f09d7654d1ef027f6f8a241d282
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-07-04 12:22:03 +0000

    chore(cloudimgs): add ubuntu 24 img

--- tuana9a/vn/cobi/112-neomorph/cloudimgs/cloudimgs.tf
+++ tuana9a/vn/cobi/112-neomorph/cloudimgs/cloudimgs.tf
@@ -1,6 +1,7 @@
 locals {
   cloudimgs = [
-    { source_file = { path = "https://cloud.debian.org/images/cloud/bookworm/20260510-2474/debian-12-generic-amd64-20260510-2474.qcow2" } }
+    { source_file = { path = "https://cloud.debian.org/images/cloud/bookworm/20260510-2474/debian-12-generic-amd64-20260510-2474.qcow2" } },
+    { source_file = { path = "https://cloud-images.ubuntu.com/noble/20260615/noble-server-cloudimg-amd64.img" } }
   ]
 }
 
@@ -12,7 +13,7 @@ resource "proxmox_virtual_environment_file" "neomorph" {
   node_name    = "neomorph"
 
   source_file {
-    file_name = "${split(".", basename(each.value.source_file.path))[0]}.img"
+    file_name = lookup(each.value, "file_name", "${split(".", basename(each.value.source_file.path))[0]}.img")
     path      = each.value.source_file.path
   }
 }
@@ -25,7 +26,7 @@ resource "proxmox_virtual_environment_file" "engineer" {
   node_name    = "engineer"
 
   source_file {
-    file_name = "${split(".", basename(each.value.source_file.path))[0]}.img"
+    file_name = lookup(each.value, "file_name", "${split(".", basename(each.value.source_file.path))[0]}.img")
     path      = each.value.source_file.path
   }
 }
