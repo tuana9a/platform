9bef4636ec110ae41665dbd7b816a218053fffeb
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-09-01 05:07:13 +0000

    change(wireguard-clients): re-add starscream

--- tuana9a/vn/hanoi/121-anor-londo-cluster/20-wireguard-clients/starscream.tf
+++ tuana9a/vn/hanoi/121-anor-londo-cluster/20-wireguard-clients/starscream.tf
@@ -1,3 +1,11 @@
-# "starscream-0.conf" = data.vault_kv_secret_v2.wireguard_clients.data.starscream-0_conf
-# "starscream-1.conf" = data.vault_kv_secret_v2.wireguard_clients.data.starscream-1_conf
-# "starscream-2.conf" = data.vault_kv_secret_v2.wireguard_clients.data.starscream-2_conf
+module "starscream-clients" {
+  source    = "./modules/wg-client"
+  name      = "starscream-clients"
+  namespace = kubernetes_namespace_v1.wireguard.metadata[0].name
+  wg_configs = [
+    data.vault_kv_secret_v2.wireguard_clients.data.starscream-0_conf,
+    data.vault_kv_secret_v2.wireguard_clients.data.starscream-1_conf,
+    data.vault_kv_secret_v2.wireguard_clients.data.starscream-2_conf,
+  ]
+  replicas = 2
+}
