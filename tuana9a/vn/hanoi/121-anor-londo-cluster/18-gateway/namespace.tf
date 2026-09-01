a66e8cb0737fbcbe6d5d9f63a262e385f640c958
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-09-01 12:26:29 +0000

    change(gateway): moving envoy gateways to envoy-gateway-system namespace

--- tuana9a/vn/hanoi/121-anor-londo-cluster/18-gateway/namespace.tf
+++ tuana9a/vn/hanoi/121-anor-londo-cluster/18-gateway/namespace.tf
@@ -3,3 +3,9 @@ resource "kubernetes_namespace_v1" "gateway" {
     name = "gateway"
   }
 }
+
+data "kubernetes_namespace_v1" "envoy_gateway_system" {
+  metadata {
+    name = "envoy-gateway-system"
+  }
+}
--- tuana9a/vn/hanoi/121-anor-londo-cluster/18-gateway/starscream-gw.tf
+++ tuana9a/vn/hanoi/121-anor-londo-cluster/18-gateway/starscream-gw.tf
@@ -1,12 +1,14 @@
 locals {
-  namespace = kubernetes_namespace_v1.gateway.metadata[0].name
+  envoy_gateway_ns   = data.kubernetes_namespace_v1.envoy_gateway_system.metadata[0].name
+  starscream_gw_yaml = templatefile("./starscream-gw.yml", { namespace = local.envoy_gateway_ns })
   starscream_gw_manifests = {
-    for i, x in split("---", templatefile("./starscream-gw.yml", { namespace = local.namespace })) :
+    for i, x in split("---", local.starscream_gw_yaml) :
     i => yamldecode(x)
   }
 }
 
 resource "kubernetes_manifest" "starscream_gw" {
   for_each = local.starscream_gw_manifests
+
   manifest = each.value
 }
