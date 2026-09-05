a1f83f956012a2267594ba286e12b15483301727
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-09-05 08:54:03 +0000

    refactor: move falco (old name: security) to hanoi folder

---
+++ tuana9a/vn/hanoi/121-anor-londo-cluster/24-falco/terraform.tf
@@ -0,0 +1,61 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-tuana9a"
+    prefix = "1788596890" # date +%s
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.29.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.1.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "2.3.5"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 5.10.1"
+    }
+  }
+}
+
+provider "google" {
+  project = "tuana9a"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-b"
+}
+
+provider "vault" {
+  address          = "https://vault.tuana9a.com"
+  skip_child_token = true
+}
+
+ephemeral "vault_kv_secret_v2" "cluster_auth" {
+  mount = "kvv2"
+  name  = "in-cluster/namespaces/default/serviceaccounts/zeus"
+}
+
+provider "kubernetes" {
+  host                   = "https://192.168.56.21:6443"
+  cluster_ca_certificate = base64decode(ephemeral.vault_kv_secret_v2.cluster_auth.data["cluster_ca_certificate_b64"])
+  token                  = ephemeral.vault_kv_secret_v2.cluster_auth.data["cluster_auth_token"]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://192.168.56.21:6443"
+    cluster_ca_certificate = base64decode(ephemeral.vault_kv_secret_v2.cluster_auth.data["cluster_ca_certificate_b64"])
+    token                  = ephemeral.vault_kv_secret_v2.cluster_auth.data["cluster_auth_token"]
+  }
+}
+
+provider "external" {
+}
