bbf142ae0a1ad6aeb82c2dad10982777b491a136
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-09-06 06:58:05 +0000

    chore(cilium): splited cilium to standalone folder

---
+++ tuana9a/vn/hanoi/121-anor-londo-cluster/02-cilium/cilium.tf
@@ -0,0 +1,27 @@
+import {
+  to = kubernetes_namespace_v1.cilium_system
+  id = "cilium-system"
+}
+
+import {
+  to = helm_release.cilium
+  id = "cilium-system/cilium"
+}
+
+resource "kubernetes_namespace_v1" "cilium_system" {
+  metadata {
+    name = "cilium-system"
+  }
+}
+
+resource "helm_release" "cilium" {
+  name      = "cilium"
+  namespace = kubernetes_namespace_v1.cilium_system.metadata[0].name
+
+  repository = "https://helm.cilium.io/"
+  chart      = "cilium"
+  version    = "1.19.1"
+  values     = [file("./values.yml")]
+
+  timeout = 10 * 60
+}
---
+++ tuana9a/vn/hanoi/121-anor-londo-cluster/02-cilium/terraform.tf
@@ -0,0 +1,54 @@
+terraform {
+  backend "gcs" {
+    bucket = "terraform-tuana9a"
+    prefix = "1788671971" # date +%s
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "8.1.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.2.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.3.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "5.11.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "tuana9a"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-b"
+}
+
+provider "vault" {
+  address          = "https://vault.tuana9a.com"
+  skip_child_token = true
+}
+
+ephemeral "vault_kv_secret_v2" "cluster_auth" {
+  mount = "kvv2"
+  name  = "in-cluster/namespaces/default/serviceaccounts/zeus"
+}
+
+provider "kubernetes" {
+  host                   = "https://192.168.56.21:6443"
+  cluster_ca_certificate = base64decode(ephemeral.vault_kv_secret_v2.cluster_auth.data["cluster_ca_certificate_b64"])
+  token                  = ephemeral.vault_kv_secret_v2.cluster_auth.data["cluster_auth_token"]
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = "https://192.168.56.21:6443"
+    cluster_ca_certificate = base64decode(ephemeral.vault_kv_secret_v2.cluster_auth.data["cluster_ca_certificate_b64"])
+    token                  = ephemeral.vault_kv_secret_v2.cluster_auth.data["cluster_auth_token"]
+  }
+}
