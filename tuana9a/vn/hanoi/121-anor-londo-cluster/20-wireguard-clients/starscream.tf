78b5f79b5c7a5478774cb5196d660421e4ea53a8
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-09-06 06:55:55 +0000

    change(wireguard-clients): scale starscream replicas to 3

--- tuana9a/vn/hanoi/121-anor-londo-cluster/20-wireguard-clients/starscream.tf
+++ tuana9a/vn/hanoi/121-anor-londo-cluster/20-wireguard-clients/starscream.tf
@@ -7,5 +7,5 @@ module "starscream" {
     data.vault_kv_secret_v2.wireguard_clients.data.starscream-1_conf,
     data.vault_kv_secret_v2.wireguard_clients.data.starscream-2_conf,
   ]
-  replicas = 2
+  replicas = 3
 }
