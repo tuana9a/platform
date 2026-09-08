2ef6ea77678a5f34f4e33771880f33a23aab7620
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-09-08 00:54:22 +0000

    change(anor-londo-cluster): support kubectl label node after cluster provisioning, code refactor

--- tuana9a/vn/hanoi/121-anor-londo-cluster/cluster.tf
+++ tuana9a/vn/hanoi/121-anor-londo-cluster/cluster.tf
@@ -1,18 +1,28 @@
 locals {
+  vm_username = "u"
+
   cluster = {
     # construct cluster from inventory.yml
     # mostly keeping their own original value
     # adding just deriviated fields
-    for vmip, vm in yamldecode(file("./inventory.yml"))["cluster"]["hosts"] :
-    vm["nodename"] => merge(vm, {
-      ip_address       = vmip
-      address          = "${vmip}/24"
-      network_device   = vm["pve_network_device"]
-      is_control_plane = contains(vm.roles, "control-plane")
+    for host_ip, host in yamldecode(file("./inventory.yml"))["cluster"]["hosts"] :
+    host["nodename"] => merge(host, {
+      ip_address       = host_ip
+      address          = "${host_ip}/24"
+      network_device   = host["pve_network_device"]
+      is_control_plane = contains(host.roles, "control-plane")
+      kube_labels = [
+        "topology.kubernetes.io/region=${host.pve_cluster}",
+        "topology.kubernetes.io/zone=${host.pve_node}",
+      ]
     })
   }
 
   cluster_control_planes = { for k, v in local.cluster : k => v if v.is_control_plane }
+  cluster_kube_labels    = { for k, v in local.cluster : k => v.kube_labels }
+
+  first_control_plane_ip     = values(local.cluster_control_planes)[0].ip_address
+  kubectl_label_nodes_script = join("\n", [for k, v in local.cluster_kube_labels : "kubectl label node ${k} ${join(" ", v)}"])
 }
 
 data "vault_kv_secret_v2" "ci" {
@@ -24,12 +34,23 @@ data "external" "kubeadm_join_command" {
   program = ["bash", "${path.module}/get_join_command.sh"]
 
   query = {
-    host     = values(local.cluster_control_planes)[0].ip_address
-    ssh_user = "u"
+    host     = local.first_control_plane_ip
+    ssh_user = local.vm_username
 
     ssh_key_content = data.vault_kv_secret_v2.ci.data.id_rsa
   }
 }
+
+# data "external" "kube_certs" {
+#   program = ["bash", "${path.module}/get_kubeadm_certs.sh"]
+
+#   query = {
+#     host     = local.first_control_plane_ip
+#     ssh_user = local.vm_username
+
+#     ssh_key_content = data.vault_kv_secret_v2.ci.data.id_rsa
+#   }
+# }
 
 locals {
   kubeadm_join_command = data.external.kubeadm_join_command.result.join_command
@@ -100,14 +121,14 @@ resource "proxmox_virtual_environment_vm" "cluster" {
 
     user_account {
       password = random_password.vm_password.result
-      username = "u"
+      username = local.vm_username
       keys     = var.vm_authorized_keys
     }
   }
 
   connection {
     type        = "ssh"
-    user        = "u"
+    user        = local.vm_username
     private_key = data.vault_kv_secret_v2.ci.data.id_rsa
     host        = each.value.ip_address
   }
@@ -117,16 +138,44 @@ resource "proxmox_virtual_environment_vm" "cluster" {
     destination = "/tmp/install-kube.sh"
   }
 
+  provisioner "file" {
+    content     = <<EOF
+#!/bin/bash
+
+set -euo pipefail
+
+export KUBERNETES_VERSION=${each.value.kubernetes_version}
+chmod +x /tmp/install-kube.sh && /tmp/install-kube.sh
+sudo ${local.kubeadm_join_command} ${each.value.is_control_plane ? "--control-plane" : ""}
+EOF
+    destination = "/tmp/kube-setup.sh"
+  }
+
   provisioner "remote-exec" {
     inline = [
       "#!/bin/bash",
       "set -euo pipefail",
-      "export KUBERNETES_VERSION=${each.value.kubernetes_version}",
-      "chmod +x /tmp/install-kube.sh",
-      "/tmp/install-kube.sh",
-      "sudo ${local.kubeadm_join_command} ${each.value.is_control_plane ? "--control-plane" : ""}"
+      "chmod +x /tmp/kube-setup.sh && /tmp/kube-setup.sh",
     ]
   }
+
+  #   provisioner "local-exec" {
+  #     command = <<EOF
+  # #!/usr/bin/env bash
+  # set -euo pipefail
+
+  # # Write the key content to a temp file - ssh requires a file, not inline content
+  # KEY_FILE=$(mktemp)
+  # trap 'rm -f "$${KEY_FILE}"' EXIT
+
+  # printf '%s\n' "${data.vault_kv_secret_v2.ci.data.id_rsa}" > "$${KEY_FILE}"
+  # chmod 600 "$${KEY_FILE}"
+
+  # ssh -o StrictHostKeyChecking=no \
+  # -i "$${KEY_FILE}" \
+  # "${local.vm_username}@${each.value.ip_address}"
+  # EOF
+  #   }
 
   on_boot = true
 
@@ -134,5 +183,34 @@ resource "proxmox_virtual_environment_vm" "cluster" {
 
   lifecycle {
     ignore_changes = [initialization, disk[0].file_id]
+  }
+}
+
+resource "terraform_data" "post_cluster_setup" {
+  triggers_replace = local.cluster_kube_labels
+
+  connection {
+    type        = "ssh"
+    user        = local.vm_username
+    private_key = data.vault_kv_secret_v2.ci.data.id_rsa
+    host        = local.first_control_plane_ip
+  }
+
+  provisioner "file" {
+    destination = "/tmp/kubectl_label_nodes.sh"
+    content     = <<EOF
+#!/usr/bin/env bash
+set -euo pipefail
+export KUBECONFIG="$${KUBECONFIG:-/etc/kubernetes/admin.conf}"
+${local.kubectl_label_nodes_script}
+EOF
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "#!/usr/bin/env bash",
+      "set -exuo pipefail",
+      "chmod +x /tmp/kubectl_label_nodes.sh && sudo /tmp/kubectl_label_nodes.sh",
+    ]
   }
 }
