e87ffe2be13d3452fdc48fbf74f5f3518af8b260
Nguyen Minh Tuan <tuana9a@gmail.com>
2026-09-15 00:46:44 +0000

    change(aws-iam): rename to upload-tuana9a-web

--- tuana9a/global/005-aws-iam/upload-tuana9a-web.tf
+++ tuana9a/global/005-aws-iam/upload-tuana9a-web.tf
@@ -1,7 +1,7 @@
 # https://docs.github.com/en/actions/security-for-github-actions/security-hardening-your-deployments/configuring-openid-connect-in-amazon-web-services#configuring-the-role-and-trust-policy
 
-resource "aws_iam_role" "upload_s3_tuana9a_com" {
-  name               = "upload-s3-tuana9a-com"
+resource "aws_iam_role" "upload_tuana9a_web" {
+  name               = "upload-tuana9a-web"
   assume_role_policy = <<EOT
 {
   "Version": "2012-10-17",
@@ -14,7 +14,7 @@ resource "aws_iam_role" "upload_s3_tuana9a_com" {
       },
       "Condition": {
         "StringLike": {
-          "token.actions.githubusercontent.com:sub": "repo:tuana9a/tuana9a.com:*"
+          "token.actions.githubusercontent.com:sub": "repo:tuana9a/web:*"
         },
         "StringEquals": {
           "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
@@ -26,13 +26,13 @@ resource "aws_iam_role" "upload_s3_tuana9a_com" {
 EOT
 }
 
-resource "aws_iam_role_policy" "upload_s3_tuana9a_com" {
-  role   = aws_iam_role.upload_s3_tuana9a_com.name
-  name   = "upload-s3-tuana9a-com"
-  policy = data.aws_iam_policy_document.upload_s3_tuana9a_com.json
+resource "aws_iam_role_policy" "upload_tuana9a_web" {
+  role   = aws_iam_role.upload_tuana9a_web.name
+  name   = "upload-tuana9a-web"
+  policy = data.aws_iam_policy_document.upload_tuana9a_web.json
 }
 
-data "aws_iam_policy_document" "upload_s3_tuana9a_com" {
+data "aws_iam_policy_document" "upload_tuana9a_web" {
   statement {
     effect = "Allow"
     actions = [
